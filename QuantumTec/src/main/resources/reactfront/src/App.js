c2744f423e6e4c75942a8864d2c556c4f8c433b2
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-04-30 17:11:49 +0900

    init: 리엑트 스프링부트 기본 세팅

---
+++ QuantumTec/src/main/resources/reactfront/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useEffect , useState} from "react";
+import axios from 'axios';
+
+function App() {
+  const [hello, setHello] = useState('');
+
+  useEffect(()=>{
+    axios.get('/api/hello')
+        .then(res => setHello(res.data))
+        .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div className="App">
+      백엔드에서 가져온 데이터 : {hello}
+    </div>
+  );
+}
+
+export default App;
---
+++ QuantumTec/src/test/java/com/project/quantumtec/QuantumTecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.quantumtec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumTecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
