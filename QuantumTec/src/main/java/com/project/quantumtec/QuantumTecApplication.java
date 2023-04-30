0716958b725f94245a147930ae057b135e65e33d
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-04-30 17:11:49 +0900

    init: 리엑트 스프링부트 기본 세팅

---
+++ QuantumTec/src/main/java/com/project/quantumtec/QuantumTecApplication.java
@@ -0,0 +1,13 @@
+package com.project.quantumtec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumTecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuantumTecApplication.class, args);
+    }
+
+}
