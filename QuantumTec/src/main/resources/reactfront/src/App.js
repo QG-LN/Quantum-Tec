28ef964152350d1b32af1a88574a36731650a188
saetu0422 <junsoi0422@gmail.com>
2023-05-02 10:29:30 +0900

    템플릿페이지

--- QuantumTec/src/main/resources/reactfront/src/App.js
+++ QuantumTec/src/main/resources/reactfront/src/App.js
@@ -1,20 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useEffect , useState} from "react";
-import axios from 'axios';
+import './styles.css';
 
 function App() {
-  const [hello, setHello] = useState('');
-
-  useEffect(()=>{
-    axios.get('/api/hello')
-        .then(res => setHello(res.data))
-        .catch(err => console.log(err));
-  }, []);
-
   return (
     <div className="App">
-      백엔드에서 가져온 데이터 : {hello}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
