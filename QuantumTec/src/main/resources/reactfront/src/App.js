4f5bc2ea933ecc69e660c70fd49e10e5cb20abb9
Junsoi <83137169+saetu0422@users.noreply.github.com>
2023-05-07 18:20:11 +0900

    fristcommit
    화면 넘어가는 기능 및 메인화면 구도 변경

--- QuantumTec/src/main/resources/reactfront/src/App.js
+++ QuantumTec/src/main/resources/reactfront/src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {useEffect , useState} from "react";
-import axios from 'axios';
+import './styles.css';
+import  Navbar from './component/navbar.js';
+import Header from './component/header.js';
+import Section from './component/section.js';
+import Aside from './component/asideLogin';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
 
+const ClickLogin = () => {
+  document.location.href = "/login";
+}
 function App() {
-  const [hello, setHello] = useState('');
-
-  useEffect(()=>{
-    axios.get('/api/hello')
-        .then(res => setHello(res.data))
-        .catch(err => console.log(err));
-  }, []);
-
   return (
-    <div className="App">
-      백엔드에서 가져온 데이터 : {hello}
+   <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+
+function Home() {
+  return (
+    <div className="Home">
+          {/*위에 광고 화면과 로그인 크기 맞추기위해 설정해놓음*/}
+        <div style={{ display: 'flex' }}>
+        <header style={{ flexBasis: '70%' }}>
+          <Header />
+        </header>
+        <aside style={{ flexBasis: '30%' }}>
+          <Aside />
+        </aside>
+      </div>
+      <Section />
+        </div>
+  );
+}
+
+function Login(){
+  return (
+    <div className="Login">
+      <Aside />
     </div>
   );
 }
+
+
+
 
 export default App;
