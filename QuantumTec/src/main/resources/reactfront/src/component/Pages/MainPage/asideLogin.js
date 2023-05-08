7869bc531b929356b30853a8fda67adbaf6ec222
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-05-08 23:53:21 +0900

    feat: 로그인 뷰 관련 코드 수정

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/MainPage/asideLogin.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/MainPage/asideLogin.js
@@ -1,15 +1,35 @@
 import Userimg from './asideImg.js';
 import '../../../App.css';
 import '../../../styles.css';
+import React, {useEffect , useState} from "react";
+import axios from 'axios';
+
+import Login from '../LoginPage/login.js';
 
 export default function AsideLogin() {
+    const [userData, setUserData] = useState('')
 
+    useEffect(()=>{
+        const inputId = localStorage.getItem("inputId");
+        const inputPw = localStorage.getItem("inputPw");
+        axios.post('/login', {
+            userID: inputId,
+            userPW: inputPw,
+        })
+            .then(res=>{
+                setUserData(res.data);
+            })
+            .catch(err => console.log(err));
+    },[]);
 
     /*로그인 확인 임시 false 비로그인/true 로그인*/
-    const truelogin = true;
-    
-    const username = '안녕하세요경민대학교';
-    const usercash = '10000';
+    let truelogin = false;
+
+    if(userData !== null) truelogin = true;
+
+
+    const username = userData.userNickname;
+    const usercash = userData.userPostal;
     const ClickLogin = () => {
         document.location.href = "/login";
       }
@@ -20,18 +40,18 @@ export default function AsideLogin() {
                 {/*로그인상태가 아닐때*/}
                 {!truelogin && <div class="col-sm">
                     <button type="button" class="btn btn-primary btn-lg btn-block btnstyle" onClick={ClickLogin} >Login</button>
-                </div>}               
-                {/*로그인상태일때*/}               
+                </div>}
+                {/*로그인상태일때*/}
                 {truelogin &&<div class="col-sm flex" >
                     <span class='inline ml-4 pt-5 mt-5 loginstyle'><div>{username}</div>{usercash}</span>
                     <span class='inline'><Userimg /></span>
                 </div> }
-                
+
             </div>
         </div>
 
 
-        
+
         </>
     );
 
