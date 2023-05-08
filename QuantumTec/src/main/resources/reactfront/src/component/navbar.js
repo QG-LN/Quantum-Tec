5dc8f1d1b6942540586786d80e3fcf33e00bfcea
Junsoi <83137169+saetu0422@users.noreply.github.com>
2023-05-08 11:46:37 +0900

    로그인 후 화면 설정 및 css 정리 작업
    navbar.js
    * 오른쪽 상단 버튼 복구
    * truelogin으로 로그인 상태를 변경할 수 있음
    * username으로 유저닉네임을 임시 'test'로 설정
    * 비로그인 상태이면 로그인으로 표시 / 로그인 상태이면 유저닉네임으로 표시
    * 비로그인 상태시 로그인 버튼을 누르면 로그인 페이지로 이동할 수 있도록 설정
    
    asideLogin.js
    * truelogin으로 로그인 상태를 변경할 수 있음
    * username으로 유저닉네임을 임시 'test'로 설정
    * usercash으로 유저의금액을 임시 '10000'으로 설정
    * 비로그인 상태이면 로그인 버튼표시 / 로그인 상태이면 user의 이름과 캐시 아바타를 표시
    * import Userimg을 사용하여 유저의 캐릭터 이미지를 표시
    * 로그인 상태일때 글자 위치와 이미지 위치 설정 (css부분은 loginstyle에 작성되어있음)
    
    style.css
    * 필요한 css부분을 맨 아래 제작
    * header-container : 적응형으로 바꾸기위해 max-width으로 변경
    * flex : display를 가로로 두기위해 flex선언
    * loginstyle : login후 aside부분 스타일
    * btnstyle : btn의 크기 설정
    * userimg > label > .imgstyle : 유저의 캐릭터 이미지 사이즈
    #New
    asideImg.js
    * 새롭게 생성
    * 유저의 캐릭터 이미지를 가져올 파일
    * 임시로 이미지를 집어넣는 방법으로 구현

--- QuantumTec/src/main/resources/reactfront/src/component/navbar.js
+++ QuantumTec/src/main/resources/reactfront/src/component/navbar.js
@@ -1,4 +1,10 @@
+import '../App.css';
 export default function Navbar() {
+    const truelogin = false;
+    const username = 'test';
+    const ClickLogin = () => {
+        document.location.href = "/login";
+      }
            return (
             <div>
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -8,7 +14,7 @@ export default function Navbar() {
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                              {/*카테고리*/}
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4 navbar-nav-right">
                                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="/">Home</a></li>
                                 <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
                                 <li class="nav-item dropdown">
@@ -23,7 +29,16 @@ export default function Navbar() {
                             </ul>
                             {/*우측 상단에 넣을수 있는 자리(필요시 로그인버튼을 간소화할떄 쓸 위치)*/}
                             <form class="d-flex">
-                            </form>
+                                {!truelogin && <button class="btn btn-outline-dark" type="button" onClick={ClickLogin} >
+                                <i class="loginbtn me-1"></i>
+                                    로그인
+                                </button>}
+                                {truelogin && <button class="btn btn-outline-dark" type="submit">
+                                <i class="userbtn me-1"></i>
+                                    {username}
+                                </button>}
+                            </form>       
+
                         </div>
                     </div>
                 </nav>
