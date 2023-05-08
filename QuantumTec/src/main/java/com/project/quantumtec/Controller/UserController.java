10114474cc44a802b8b475eb3ea13a8fdc517c46
YeChanKang <yechan.dev@gmail.com>
2023-05-09 08:52:52 +0900

    fix: [#7] 로그인 기능 수정

--- QuantumTec/src/main/java/com/project/quantumtec/Controller/UserController.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Controller/UserController.java
@@ -3,14 +3,19 @@ package com.project.quantumtec.Controller;
 import com.project.quantumtec.Service.login.LoginService;
 import com.project.quantumtec.VO.user.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping ("/api/login")
 public class UserController {
 
     @Autowired
     private LoginService loginService;
 
+    @PostMapping("/testo")
+    public UserVO testo(@RequestParam String userID, @RequestParam String userPW) throws Exception {
+        return login(userID, userPW);
+    }
 
     public UserVO login(String userID, String userPW) throws Exception {
         UserVO checkUser = loginService.login(userID, userPW);
