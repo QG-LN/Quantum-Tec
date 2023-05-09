7ec5fa1b6e9b493500ba90a923255d2840173387
Junsoi <83137169+saetu0422@users.noreply.github.com>
2023-05-09 14:08:52 +0900

    #4 중복체크와 빠진 부분 추가
    * 비밀번호 중복체크추가
    * 이메일 인증버튼 및 인증확인버튼 추가
    * 우편번호 텍스트 추가
    * 각 조건별 크기 조건 추가
    * 조건에 맞지 않을 시 빨간 글씨 추가
    * 버튼 종류 추가

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/LoginPage/sign.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/LoginPage/sign.js
@@ -4,15 +4,20 @@ import axios from 'axios';
 export default function Sign(){
     const [inputName, setInputName] = useState('')
     const [inputBirth, setInputBirth] = useState('')
+    const [inputNickname, setInputNickname] = useState('')
     const [inputId, setInputId] = useState('')
     const [inputPw, setInputPw] = useState('')
-    const [inputPwCheack, setInputPwCheack] = useState('')
+    const [inputPwCheck, setInputPwCheck] = useState('')
     const [inputEmail, setInputEmail] = useState('')
     const [inputEmailCheck, setInputEmailCheck] = useState('')
     const [inputAddress, setInputAddress] = useState('')
     const [inputAddressDetail, setInputAddressDetail] = useState('')
     const [inputRole, setInputRole] = useState('')
+    const [showEmailCheck, setshowEmailCheck] = useState(false)
+    const [inputPostAddress, setInputPostAddress] = useState('')
 
+    //이메일과 이메일인증 버튼의 Disabled 속성 확인
+    const [inputEmailDisabled, setInputEmailDisabled] = useState(false);
  
 	// input data 의 변화가 있을 때마다 value 값을 변경해서 useState 해준다
     const handleInputName = (e) => {
@@ -29,7 +34,7 @@ export default function Sign(){
         setInputPw(e.target.value)
     }
     const handleInputPwCheck = (e) => {
-        setInputPwCheack(e.target.value)
+        setInputPwCheck(e.target.value)
     }
 
     const handleInputEmail = (e) => {
@@ -49,18 +54,55 @@ export default function Sign(){
     const handleInputEmailCheck = (e) => {
         setInputEmailCheck(e.target.value)
     }
-    const OnClickEmailSend = () => {
-        console.log(inputEmail)
+    const handleInputNickname = (e) => {
+        setInputNickname(e.target.value)
+    }
+    const handleInputPostAddress = (e) => {
+        setInputPostAddress(e.target.value)
     }
 
+    // 아이디 체크 버튼 클릭 이벤트
+    const OnClickIdCheck = () => {
+        console.log(inputId)
+    }
+
+    // 닉네임 체크 버튼 클릭 이벤트
+    const OnClickNicknameCheck = () => {
+        console.log(inputNickname)
+    }
+
+    // 이메일 인증 버튼 클릭 이벤트
+    const OnClickEmailSend = () => {
+        //이메일과 이메일 인증버튼 비활성화
+        setInputEmailDisabled(true);
+        //이메일 인증 텍스트와 인증버튼 활성화
+        setshowEmailCheck(true);
+    }
+
+    //인증번호 체크 버튼 클릭 이벤트
     const OnClickEmailCheck = () => {
         console.log(inputEmailCheck)
+
     }
-	// login 버튼 클릭 이벤트
+
+    const OnClickAddress = () => {
+        console.log(inputAddress)
+    }
 
     // signup 버튼 클릭 이벤트
     const OnClickSignUp = () => {
-        console.log(inputName, inputBirth, inputId, inputPw, inputPwCheack, inputEmail, inputAddress, inputAddressDetail, inputRole)
+        console.log(inputName, inputBirth, inputId, inputPw, inputPwCheck, inputEmail, inputAddress, inputAddressDetail, inputRole)
+        if (inputPw !== inputPwCheck) {
+            alert('비밀번호가 일치하지 않습니다.')
+            return
+        }else if(inputPw == inputPwCheck){
+            alert('감사링')
+            return
+        }
+        
+    }
+    const OnClickCancel = () => {
+        document.location.href = "/";
     }
  
 	// 페이지 렌더링 후 가장 처음 호출되는 함수
@@ -76,37 +118,61 @@ export default function Sign(){
             <h2>회원가입</h2>
             <div class="py-3">
                 <label htmlFor='input_id'>이름 : </label>
-                <input type='text' name='input_name' maxlength="10" value={inputName} onChange={handleInputName} />
+                <input type='text' name='input_name' maxLength={20} value={inputName} onChange={handleInputName} />
+                {inputName.length > 20 && (
+                        <div style={{ color: 'red' }}>이름을 20글자 이내로 써주세요</div>
+                    )}
             </div>
-            <div class ='py-3'>
+            <div class ='py-1'>
                 <label htmlFor='input_date'>생년월일 : </label>
                 <input type='date' name='input_birth' value={inputBirth} onChange={handleInputBirth} />
             </div>
+            <div class='py1'>
+                <label htmlFor='input_nickname'>닉네임 : </label>
+                <input type='text' name='input_nickname' maxLength={8} value={inputNickname} onChange={handleInputNickname} />
+                <button type="button" onClick={OnClickNicknameCheck}>중복확인</button>
+                {inputNickname.length > 8 && (
+                        <div style={{ color: 'red' }}>닉네임을 8글자 이내로 써주세요</div>
+                    )}
+            </div>
             <div class="py-1">
                 <label htmlFor='input_id'>ID : </label>
-                <input type='text' name='input_id'maxlength="10" value={inputId} onChange={handleInputId} />
+                <input type='text' name='input_id'maxLength={20} value={inputId} onChange={handleInputId} />
+                <button type="button" onClick={OnClickIdCheck}>중복확인</button>
+                {inputId.length > 20 && (
+                        <div style={{ color: 'red' }}>아이디를 20글자 이내로 써주세요</div>
+                    )}
             </div>
             <div class ="py-1">
                 <label htmlFor='input_pw'>PW : </label>
-                <input type='password' name='input_pw' value={inputPw} onChange={handleInputPw} />
+                <input type='password' name='input_pw' minLength={8} value={inputPw} onChange={handleInputPw} />
+                {inputPw.length <= 8 && (
+                        <div style={{ color: 'red' }}>패스워드를 8자 이상 써주세요</div>
+                    )}
             </div>
             <div class ="py-1">
                 <label htmlFor='input_pw'>PW확인 : </label>
-                <input type='password' name='check_input_pw' value={inputPwCheack} onChange={handleInputPwCheck} />
+                <input type='password' name='check_input_pw' minLength={8} value={inputPwCheck} onChange={handleInputPwCheck} />
             </div>
+
             <div class="py-1">
                 <label htmlFor='input_email'>Email : </label>
-                <input type='email' name='input_email' value={inputEmail} onChange={handleInputEmail} />
-                <button type="button" onClick={OnClickEmailSend}>인증번호 전송</button>
+                <input type='email' name='input_email' value={inputEmail} onChange={handleInputEmail} disabled={inputEmailDisabled}/>
+                <button type="button" onClick={OnClickEmailSend} disabled={inputEmailDisabled}>인증번호 전송</button>
             </div>
-            <div>
+            {showEmailCheck && <div>
                 <label htmlFor='input_email'>인증번호 : </label>
-                <input type='text' name='input_email_check' value={inputEmailCheck} onChange={handleInputEmailCheck} />
+                <input type='text' name='input_email_check' value={inputEmailCheck} onChange={handleInputEmailCheck}/>
                 <button type="button" onClick={OnClickEmailCheck}>인증번호 확인</button>
-            </div>
+            </div>}
             <div class="py-1">
                 <label htmlFor='input_address'>주소 : </label>
-                <input type='text' name='input_address' value={inputAddress} onChange={handleInputAddress}/>
+                <input type='text' name='input_address' value={inputAddress} onChange={handleInputAddress} readOnly/>
+                <button type="button" onClick={OnClickAddress}>주소검색</button>
+            </div>
+            <div class="py-1">
+                <label htmlFor='input_postaddress'>우편번호 : </label>
+                <input type='text' name='input_postaddress' value={inputPostAddress} onChange={handleInputPostAddress} readOnly/>
             </div>
             <div class="py-1">
                 <label htmlFor='input_address_D'>상세주소 : </label>
@@ -114,13 +180,14 @@ export default function Sign(){
             </div>
 
             <div class="py-1">
-                <input type='radio' name='input_role' value='depeloper' onChange={handleInputRole} />공급자
                 <input type='radio' name='input_role' value='uesr'  onChange={handleInputRole} />사용자
+                <input type='radio' name='input_role' value='depeloper' onChange={handleInputRole} />공급자
             </div>
 
 
             <div class="py-1">
-                <button type="button" onClick={OnClickSignUp}>SignUp</button>
+                <button type="button" onClick={OnClickSignUp}>회원가입</button>
+                <button type='buuton' onClick={OnClickCancel}>취소</button>
             </div>
         </div>
     );
