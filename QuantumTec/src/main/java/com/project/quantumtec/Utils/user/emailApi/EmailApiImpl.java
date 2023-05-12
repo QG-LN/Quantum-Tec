f9f7ea6d1a37947a4862f93ee31976868a1e2927
MayoneJY <mayone6063@kakao.com>
2023-05-12 22:10:12 +0900

    feat: [#4] Email 인터페이스 생성

---
+++ QuantumTec/src/main/java/com/project/quantumtec/Utils/user/emailApi/EmailApiImpl.java
@@ -0,0 +1,25 @@
+package com.project.quantumtec.Utils.user.emailApi;
+
+public class EmailApiImpl {
+    // 이메일 전송
+    public void sendEmail(String to, String subject, String text) throws Exception {
+        
+    }
+
+    // 인증번호 생성
+    public String createKey() throws Exception {
+        
+        return "인증번호";
+    }
+
+    // 인증번호 확인
+    public boolean checkKey(String key) throws Exception {
+        
+        return true;
+    }
+
+    // 인증번호 삭제
+    public void removeKey(String key) throws Exception {
+        
+    }
+}
