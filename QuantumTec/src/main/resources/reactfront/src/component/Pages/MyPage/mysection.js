7855a1eebe31dcb3493c929e6cc00135fe4952c2
Junsoi <83137169+saetu0422@users.noreply.github.com>
2023-05-17 21:51:54 +0900

    #4 회원가입 CSS
    * 회원수정화면 변경함으로써 화원가입 CSS 변경 확인 및 수정

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/MyPage/mysection.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/MyPage/mysection.js
@@ -16,6 +16,7 @@ export default function Mysection(){
     const [inputAddress, setInputAddress] = useState('')
     const [inputAddressDetail, setInputAddressDetail] = useState('')
     const [inputRole, setInputRole] = useState('')
+    const [inputGender, setInputGender] = useState('men');
     const [showEmailCheck, setshowEmailCheck] = useState(false)
     const [inputPostAddress, setInputPostAddress] = useState('')
     const [isNickDiabled, setIsNickDisabled] = useState(false);
@@ -71,6 +72,9 @@ export default function Mysection(){
     }
     const handleInputPostAddress = (e) => {
         setInputPostAddress(e.target.value)
+    }
+    const handleInputGender = (e) => {
+        setInputGender(e.target.value)
     }
 
     // 아이디 체크 버튼 클릭 이벤트
@@ -136,7 +140,7 @@ export default function Mysection(){
     }
 
     // signup 버튼 클릭 이벤트
-    const OnClickSign = () => {
+    const OnClickSave = () => {
         console.log(inputName, inputBirth, inputId, inputPw, inputPwCheck, inputEmail, inputAddress, inputAddressDetail, inputRole)
         if(inputName.length > 20){
             alert('이름을 20글자 이내로 써주세요');
@@ -184,35 +188,18 @@ export default function Mysection(){
 
 
     return (
-        <div class="signup-form container">
-            <h2 class ='Logintitle'>회원정보수정</h2>
-            <div class="form-group row">
-                <div class= 'col-3'>
-                    <label htmlFor='input_id'>이름 : </label>
-                </div>
-                <div class= 'col-9'>
-                <input type='text' name='input_name' maxLength={20} style={style_inputbox_2} value={inputName} onChange={handleInputName} />
-                <div class = 'infotxt'>20자 이내로 적어주세요.</div>
-                {inputName.length > 20 && (
-                        <div style={{ color: 'red' }}>이름을 20글자 이내로 써주세요</div>
-                    )}
-                </div>
-            </div>
-            <div class ='form-group row'>
-                <div class= 'col-3'>
-                    <label htmlFor='input_date'>생년월일 : </label>
-                </div>
-                <div class= 'col-9'>
-                    <input type='date' name='input_birth' value={inputBirth} style={style_inputbox_2} onChange={handleInputBirth} />
-                </div>
-            </div>
+        <div class="signup-form container mypagestyle float-right w-mypagesection max-w-[880px] relative min-w-[700px]">
+            <h2 class='account_main_page_title '>회원 정보 수정</h2>
+            
             <div class='form-group row'>
                 <div class= 'col-3'>
                     <label htmlFor='input_nickname'>닉네임 : </label>
                 </div>
                 <div class= 'col-9'>
-                    <input style={style_inputbox} type='text' name='input_nickname' maxLength={8} value={inputNickname} onChange={handleInputNickname} />
+                    <div className='flex'>
+                    <input style={style_inputbox} className='border' type='text' name='input_nickname' maxLength={8} value={inputNickname} onChange={handleInputNickname} />
                     <button type="button" onClick={OnClickNicknameCheck} disabled={isNickDiabled}>중복확인</button>
+                    </div>
                     <div class = 'infotxt'>8자 이내로 적어주세요.</div>
                     {inputNickname.length > 8 && (
                           <div style={{ color: 'red' }}>닉네임을 8글자 이내로 써주세요</div>
@@ -224,8 +211,10 @@ export default function Mysection(){
                     <label htmlFor='input_id'>ID : </label>
                 </div>
                 <div class='col-9'>
-                    <input style={style_inputbox} type='text' name='input_id'maxLength={20} value={inputId} onChange={handleInputId} />
+                    <div className='flex'>
+                    <input style={style_inputbox} className='border' type='text' name='input_id'maxLength={20} value={inputId} onChange={handleInputId} />
                     <button type="button" disabled={isIdDisabled}  onClick={OnClickIdCheck}>중복확인</button>
+                    </div>
                     <div class = 'infotxt'>20자 이내로 적어주세요.</div>
                     {inputId.length > 20 && (
                             <div style={{ color: 'red' }}>아이디를 20글자 이내로 써주세요</div>
@@ -238,7 +227,7 @@ export default function Mysection(){
                     <label htmlFor='input_pw'>PW : </label>
                 </div>
                 <div class= 'col-9'>
-                    <input type='password' name='input_pw' minLength={8} style={style_inputbox_2} value={inputPw} onChange={handleInputPw} />
+                    <input type='password' className='border' name='input_pw' minLength={8} style={style_inputbox_2} value={inputPw} onChange={handleInputPw} />
                     <div class = 'infotxt'>특수문자 + 영문자 + 숫자로 구성된 8자 이상으로 써주세요.</div>
                     {/* {inputPw.length <= 8 && (
                             <div style={{ color: 'red' }}>패스워드를 8자 이상 써주세요</div>
@@ -250,16 +239,51 @@ export default function Mysection(){
                     <label htmlFor='input_pw'>PW확인 : </label>
                 </div>
                 <div class= 'col-9'>
-                    <input type='password' name='check_input_pw' style={style_inputbox_2} minLength={8} value={inputPwCheck} onChange={handleInputPwCheck} />
+                    <input type='password' className='border' name='check_input_pw' style={style_inputbox_2} minLength={8} value={inputPwCheck} onChange={handleInputPwCheck} />
                 </div>
             </div>
-
+            <div class="form-group row">
+                <div class= 'col-3'>
+                    <label htmlFor='input_id'>이름 : </label>
+                </div>
+                <div class= 'col-9'>
+                <input type='text' className='border' name='input_name' minLength={2} maxLength={20} style={style_inputbox_2} value={inputName} onChange={handleInputName} />
+                <div class = 'infotxt'>20자 이내로 적어주세요.</div>
+                {inputName.length > 20 && (
+                        <div style={{ color: 'red' }}>이름을 20글자 이내로 써주세요</div>
+                    )}
+                </div>
+            </div>
+            <div className="mb-1 flex justify-center form-group row">
+                <div class='col-3'>
+                    <label htmlFor='input_gender'>성별 : </label>
+                </div>
+                <div class='col-9 flex'>
+                    <div  className='w-36 text-right' >
+                        <input className='mr-4 ml-4' type='radio' name='input_gender' value='male' onChange={handleInputGender} checked/>남자
+                    </div>
+                    <div  className='w-36 text-center'>
+                        <input className='mr-4 ml-4' type='radio' name='input_gender' value='famale' onChange={handleInputGender} />여자
+                    </div>
+                    <div  className='w-36 text-left' >
+                            <input className='mr-4 ml-4' type='radio' name='input_gender' value='default' onChange={handleInputGender} />비공개
+                    </div>
+                </div>
+            </div>
+            <div class ='form-group row'>
+                <div class= 'col-3'>
+                    <label htmlFor='input_date'>생년월일 : </label>
+                </div>
+                <div class= 'col-9'>
+                    <input type='date' className='border' name='input_birth' value={inputBirth} style={style_inputbox_2} onChange={handleInputBirth} />
+                </div>
+            </div>
             <div class="form-group row">
                 <div class='col-3'>
                     <label htmlFor='input_email'>Email : </label>
                 </div>
-                <div class='col-9'>
-                    <input type='email' name='input_email' value={inputEmail} placeholder='example@google.com' style={style_inputbox} onChange={handleInputEmail} disabled={inputEmailDisabled}/>
+                <div class='col-9-button'>
+                    <input type='email'className='border' name='input_email' value={inputEmail} placeholder='example@google.com' style={style_inputbox} onChange={handleInputEmail} disabled={inputEmailDisabled}/>
                     <button type="button" onClick={OnClickEmailSend} disabled={inputEmailDisabled}>인증번호 전송</button>
                 </div>
             </div>
@@ -267,8 +291,8 @@ export default function Mysection(){
                 <div class='col-3'>
                     <label htmlFor='input_email'>인증번호 : </label>
                 </div>
-                <div class='col-9'>
-                    <input type='text' name='input_email_check' value={inputEmailCheck} style={style_inputbox} onChange={handleInputEmailCheck}/>
+                <div className='flex w-75'>
+                    <input type='text' className='border' name='input_email_check' value={inputEmailCheck} style={style_inputbox} onChange={handleInputEmailCheck}/>
                     <button type="button" onClick={OnClickEmailCheck}>인증번호 확인</button>
                 </div>
             </div>}
@@ -276,8 +300,8 @@ export default function Mysection(){
                 <div class='col-3'>
                     <label htmlFor='input_address'>주소 : </label>
                 </div>
-                <div class='col-9'>
-                    <input type='text' name='input_address' value={inputAddress} style={style_inputbox} onChange={handleInputAddress} readOnly/>
+                <div className='flex w-75'>
+                    <input type='text' className='border' name='input_address' value={inputAddress} style={style_inputbox} onChange={handleInputAddress} readOnly/>
                     <button type="button" onClick={OnClickAddress}>주소검색</button>
                 </div>
             </div>
@@ -286,7 +310,7 @@ export default function Mysection(){
                     <label htmlFor='input_postaddress'>우편번호 : </label>
                 </div>
                 <div class='col-9'>
-                    <input type='text' name='input_postaddress' value={inputPostAddress} style={style_inputbox_2} onChange={handleInputPostAddress} readOnly/>
+                    <input type='text' className='border' name='input_postaddress' value={inputPostAddress} style={style_inputbox_2} onChange={handleInputPostAddress} readOnly/>
                 </div>
             </div>
             <div class="form-group row">
@@ -294,13 +318,22 @@ export default function Mysection(){
                     <label htmlFor='input_address_D'>상세주소 : </label>
                 </div>
                 <div class='col-9'>
-                    <input type='text' name='input_address_D' style={style_inputbox_2} value={inputAddressDetail} onChange={handleInputAddressDetail}/>
+                    <input type='text' className='border' name='input_address_D' style={style_inputbox_2} value={inputAddressDetail} onChange={handleInputAddressDetail}/>
+                </div>
+            </div>
+
+            <div class="sign-button mb-2 row">
+                <div class='col-6' style={{textAlign:'right'}} >
+                    <input type='radio' name='input_role' value='user' onChange={handleInputRole} checked/>사용자
+                </div>
+                <div class='col-6' style={{textAlign:'left'}}>
+                    <input type='radio' name='input_role' value='developer' onChange={handleInputRole} />공급자
                 </div>
             </div>
 
             <div class="form-group row">
                 <div class='col-6'>
-                    <button type="button" style={bottomBtn} onClick={OnClickSign}>적용</button>
+                    <button type="button" style={bottomBtn} onClick={OnClickSave}>저장</button>
                 </div>
                 <div class='col-6'>
                     <button type='button' style={bottomBtn} onClick={OnClickCancel}>취소</button>
