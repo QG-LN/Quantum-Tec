417d4fa3e0b947af5000cd02bd495bb323aece45
Junsoi <83137169+saetu0422@users.noreply.github.com>
2023-05-17 21:35:17 +0900

    design[#9]: CSS 완성
    #9
    * 사이드바 표시 CSS변경 및 오류 해결
    * 마이페이지 기본페이지 CSS변경 및 대시보드 모양 변경
    * 전체적인 CSS변경

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/MyPage/passwordChk.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/MyPage/passwordChk.js
@@ -18,13 +18,15 @@ export default function PasswordChk(){
         };
     }
     return(
-         <div class='mysection-form'>
+         <div class='mysection-form mypagestyle float-right w-mypagesection max-w-[880px] relative min-w-[700px]'>
                 {!passwordchk ? (
                     <div class='passchk-form '>
                         <div>
-                            <h2>비밀번호확인</h2>
-                            <input className='w-6/12' type="password" name="input_pw" value={inputPw} minLength={8} onChange={handleInputPw}/>
-                            <button onClick={handleClick}>확인</button>
+                        <h2 class='account_main_page_title '>비밀번호 확인</h2>
+                            <div class="mt-[120px]">
+                                <input className='h-[50px] w-[350px] border border-black rounded-xl indent-3' placeholder='비밀번호를 입력해주세요' type="password" name="input_pw" value={inputPw} minLength={8} onChange={handleInputPw}/>
+                                <button class="w-[100px] h-[50px] ml-8 border-2 shadow-sm rounded-xl border-green-400" onClick={handleClick}>확인</button>
+                            </div>
                         </div>
                 </div>
                 ):(<Mysection />)}
