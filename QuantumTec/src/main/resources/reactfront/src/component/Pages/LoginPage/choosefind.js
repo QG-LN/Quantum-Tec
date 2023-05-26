6a2293c5ce2897c18bf89381243432a187641865
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-05-26 19:54:40 +0900

    feat[#31]: 아이디/비밀번호찾기 front-back 연결

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/LoginPage/choosefind.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/LoginPage/choosefind.js
@@ -19,6 +19,9 @@ export default function Choosefind(props) {
     const [findId,setFindId] = useState('');
     const [findPw,setFindPw] = useState('');
 
+    // 아이디 임시
+    // const auth = '1234';
+    let auth ='';
     const handleInputName = (e) => {
     setInputName(e.target.value);
   }
@@ -41,7 +44,18 @@ export default function Choosefind(props) {
         // setIsOpen(false);
         props.setModalOpen(false);
     };
-    async function checkData(path, body , methodType){
+
+
+    /**
+     * 
+     * @param path  데이터 전송 경로
+     * @param body  전송 데이터
+     * @param methodType post/get
+     * @param returnType 반환 형식
+     * @return {Promise<*|null>}
+     */
+    async function checkData(path, body , methodType, returnType){
+        returnType = returnType == undefined ? "json" : returnType;
         try {
             const response = await fetch(path, {
                 method: methodType,
@@ -52,14 +66,22 @@ export default function Choosefind(props) {
             });
 
             // response 객체가 반환될때까지 기다린후 데이터가 전달되면 json 데이터를 반환
-            const data = await response.json();             
+            let data = "";
 
-            // 반환값이 중복일 경우 true, 중복이 아닐 경우 false 이므로 !data로 반환
-            // 서버에서 받은 값이 없을 경우 false 반환
+            switch (returnType){
+                case "json":
+                    data = await response.json();
+                    break;
+                case "string" :
+                    data = await response.text();
+                    break;
+            }
+
+            // 데이터가 null이나 undefined가 아닐 경우 데이터를 반환
             if (data !== null && data !== undefined) {
-                return !data;
+                return data;
             } else {
-                return false;
+                return null;
             }
         } catch (error) {
             console.log(error);
@@ -68,64 +90,90 @@ export default function Choosefind(props) {
     }
 
     //확인 버튼 클릭 이벤트
-    const onClickIdFind = () => {
-        if(inputName === '') {
-            alert('이름을 입력해주세요');
-            return
-        // 이름 최소 길이 확인
-        }else if(inputName.length < 2){
-            alert('이름을 2글자 이상으로 써주세요');
-            return
-        }else if(inputEmail === ''){
-            alert('이메일을 입력해주세요');
-            return
-        }else if(inputEmailCheck === ''){
-            alert('인증번호를 입력해주세요');
-            return
-        }else if(inputEmailCheck != auth){
-            alert('인증번호가 일치하지 않습니다.');
-            settrueId(false);
-            return
-        }else{
-            //성공시 아이디 임시로 출력
-            console.log('성공')
-            setFindId('test1234');
-            settrueId(true);
-            
+    const onClickIdFind = async () => {
+        // 인증번호를 체크하고 그에 맞게 처리
+        const path = 'http://localhost:9090/user/signup/check-email-auth';
+        const body = { key : inputEmailCheck}
+        const data = await checkData(path,body,'POST',"json");
+        console.log(inputEmailCheck);
+        if(data){
+            if(inputName === '') {
+                alert('이름을 입력해주세요');
+                return
+            }else if(inputName.length < 2){             // 이름 최소 길이 확인
+                alert('이름을 2글자 이상으로 써주세요');
+                return
+            }else if(inputEmail === ''){
+                alert('이메일을 입력해주세요');
+                return
+            }else if(inputEmailCheck === ''){
+                alert('인증번호를 입력해주세요');
+                return
+            }else{
+                // 이름과 이메일을 통해서 아이디 가져오기
+                const path = 'http://localhost:9090/user/findid';
+                const body = {
+                    userName : inputName,
+                    userEmail : inputEmail
+                }
+                const data = await checkData(path,body,'POST',"string");    // 아이디 가져오기
+
+                // 성공적으로 아이디를 가져왔다면
+                if(data!= null){
+                    setFindId(data);
+                    settrueId(true);
+                }else{
+                    alert("아이디를 가져오는데 실패하였습니다.");
+                }
+            }
         }
+
     }
 
-    const onClickPwFind = () => {
-        if(inputName === '') {
-            alert('이름을 입력해주세요');
-            return
-        // 이름 최소 길이 확인
-        }else if(inputName.length < 2){
-            alert('이름을 2글자 이상으로 써주세요');
-            return
-        }else if(inputId === ''){
-            alert('아이디를 입력해주세요');
-            return
-        }else if(inputId.length < 21){
-            alert('아이디를 20글자 이내로 써주세요');
-            return
-        }
-        else if(inputEmail === ''){
-            alert('이메일을 입력해주세요');
-            return
-        }else if(inputEmailCheck === ''){
-            alert('인증번호를 입력해주세요');
-            return
-        }else if(inputEmailCheck != auth){
-            alert('인증번호가 일치하지 않습니다.');
-            settrueId(false);
-            return
-        }else{
-            //성공시 비밀번호 임시로 출력
-            console.log('성공')
-            setFindPw('asdfsadf!@123');
-            settruePw(true);
-            
+    const onClickPwFind = async () => {
+        // 인증번호를 체크하고 그에 맞게 처리
+        const path = 'http://localhost:9090/user/signup/check-email-auth';
+        const body = { key : inputEmailCheck}
+        const data = await checkData(path,body,'POST',"json");
+        console.log(inputEmailCheck);
+        if(data){
+            if(inputName === '') {
+                alert('이름을 입력해주세요');
+                return
+                // 이름 최소 길이 확인
+            }else if(inputName.length < 2){
+                alert('이름을 2글자 이상으로 써주세요');
+                return
+            }else if(inputId === ''){
+                alert('아이디를 입력해주세요');
+                return
+            }else if(inputId.length >= 21){
+                alert('아이디를 20글자 이내로 써주세요');
+                return
+            }else if(inputEmail === ''){
+                alert('이메일을 입력해주세요');
+                return
+            }else if(inputEmailCheck === ''){
+                alert('인증번호를 입력해주세요');
+                return
+            }else{
+                // 이름, 아이디, 이메일을 통해서 비밀번호 찾기
+                const path = 'http://localhost:9090/user/findpw';
+                const body = {
+                    userName : inputName,
+                    userID : inputId,
+                    userEmail : inputEmail,
+                }
+                const data = await checkData(path,body,'POST',"string");    // 아이디 가져오기
+
+                if(data != null){
+                    // setFindPw(data);
+                    // settruePw(true);
+                    alert('비밀번호를 이메일로 보내드렸습니다.');
+                    props.setModalOpen(false);  // 비밀번호 전송완료시 현재 모탈 창을 종료
+                }
+
+            }
         }
     }
     //아이디 찾기 버튼 클릭 이벤트
@@ -154,12 +202,18 @@ export default function Choosefind(props) {
         if(isEmail(inputEmail)){
             const path = 'http://localhost:9090/user/signup/send-email-auth';
             const body = { userEmail : inputEmail}
-            const data = await checkData(path,body,'POST');
-            setInputEmailDisabled(true);
-            //이메일 인증 텍스트와 인증버튼 활성화
-            setshowEmailCheck(true);
-            //인증번호 전송 표시
-            alert('인증번호가 전송되었습니다.');
+            const data = await checkData(path,body,'POST',"json");
+
+            if(data != null){   // 인증번호 전송에 성공하였을 경우
+                alert('인증번호가 전송되었습니다.');
+                auth = data;
+                setInputEmailDisabled(true);
+                //이메일 인증 텍스트와 인증버튼 활성화
+                setshowEmailCheck(true);
+                //인증번호 전송 표시
+            }else{
+                alert('인증번호 전송에 실패하였습니다.');
+            }
         }
         else{
             alert('이메일 형식이 올바르지 않습니다.');
@@ -168,11 +222,18 @@ export default function Choosefind(props) {
 
     //인증번호 재전송 버튼 클릭 이벤트
     const OnClickEmailReSend = async () => {
+        const path = 'http://localhost:9090/user/signup/send-email-auth';
+        const body = { userEmail : inputEmail}
+        const data = await checkData(path,body,'POST');
         alert('인증번호가 재전송되었습니다.');
+        if(data != null){
+            auth = data;
+        }else{
+            alert('인증번호 재전송에 실패하였습니다.');
+        }
+
     }
 
-   // 아이디 임시
-  const auth = '1234';
 
   return (
     <>
@@ -272,9 +333,9 @@ export default function Choosefind(props) {
                         </div>
                     </li>
                     <hr/>
-                    <li>
-                        {truePw && <div class='text-center mt-[40px]'>비밀번호는 {findPw}입니다</div>}
-                    </li>
+                    {/*<li>*/}
+                    {/*    {truePw && <div class='text-center mt-[40px]'>비밀번호는 {findPw}입니다</div>}*/}
+                    {/*</li>*/}
                 </ul>
                 </div>
             }
