71db48049bdd13875dbb51827b1a76bb3e3fcf38
YeChanKang <yechan.dev@gmail.com>
2023-05-30 10:16:26 +0900

    feat: [#23] DAO단 Try Catch로 감싸서 오류 발생시에 front단에서 작업이 진행된것으로 착각하지 못하게 수정

--- QuantumTec/src/main/java/com/project/quantumtec/DAO/user/UserDAOImpl.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DAO/user/UserDAOImpl.java
@@ -24,102 +24,154 @@ public class UserDAOImpl implements UserDAO{
     private SqlSession sqlSession;
     @Override
     public List<UserVO> getUserListAll() throws Exception {
-        System.out.println("dao: " + sqlSession.selectList("UserService.getUserListAll"));
-        return sqlSession.selectList("UserService.getUserListAll");
+        try {
+            System.out.println("dao: " + sqlSession.selectList("UserService.getUserListAll"));
+            return sqlSession.selectList("UserService.getUserListAll");
+        } catch (Exception e) {
+           return null;
+        }
     }
 
     // 사용자의 아이디와 비밀번호를 입력받아 그 값이 존재하는지 확인하고 인덱스를 반환하는 메소드
     @Override
     public int getUserExist(String userID, String userPW) throws Exception {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUserID(userID);
-        userDTO.setUserPW(userPW);
-        Integer result = sqlSession.selectOne("UserService.getUserExist", userDTO);
-        return (result == null) ? 0 : result;
+        try {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUserID(userID);
+            userDTO.setUserPW(userPW);
+            Integer result = sqlSession.selectOne("UserService.getUserExist", userDTO);
+            return (result == null) ? 0 : result;
+        } catch (Exception e) {
+            return 0;
+        }
     }
 
     // 사용자의 아이디를 입력받아 그 값이 존재하는지 확인하고 불린을 반환하는 메소드
     @Override
     public boolean isIdDuplicate(UserVO user) throws Exception {
-        return (((Integer) sqlSession.selectOne("UserService.selectCountById", user)) == null)? false : true;
+        try {
+            return (((Integer) sqlSession.selectOne("UserService.selectCountById", user)) == null)? false : true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     // 사용자의 닉네임을 입력받아 그 값이 존재하는지 확인하고 불린을 반환하는 메소드
     @Override
     public boolean isNicknameDuplicate(UserVO user) throws Exception {
-        return (((Integer) sqlSession.selectOne("UserService.selectCountByNickname", user)) == null)? false : true;
+        try {
+            return (((Integer) sqlSession.selectOne("UserService.selectCountByNickname", user)) == null)? false : true;
+        } catch (Exception e) {
+            return false;
+        }
     }
     // 사용자의 이메일을 입력받아 값이 존재할 경우 true, 존재하지 않을 경우 false를 반환하는 메소드
     @Override
     public boolean isEmailDuplicate(UserVO user) throws Exception {
-        return (((Integer) sqlSession.selectOne("UserService.selectCountByEmail", user)) == null)? false : true;
+        try {
+            return (((Integer) sqlSession.selectOne("UserService.selectCountByEmail", user)) == null)? false : true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     // 유저 인덱스를 입력받아 사용자 정보를 가져오는 함수
     @Override
     public UserInfoResponseDTO getUserInfo(int userIdx) throws Exception {
-        return sqlSession.selectOne("UserService.getUserInfo", userIdx);
+        try {
+            return sqlSession.selectOne("UserService.getUserInfo", userIdx);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @Override
     public LoginResponseDTO getLoginInfo(int userIdx) throws Exception {
-        return sqlSession.selectOne("UserService.getLoginInfo", userIdx);
+        try {
+            return sqlSession.selectOne("UserService.getLoginInfo", userIdx);
+        } catch (Exception e) {
+            return null;
+        }
     }
-    
+
     // 사용자 정보를 입력받아 DB에 저장하는 함수
     @Override
     public int setUser(UserVO user) throws Exception {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUserID(user.getUserID());
-        userDTO.setUserPW(user.getUserPW());
-        userDTO.setUserNickname(user.getUserNickname());
-        userDTO.setUserName(user.getUserName());
-        userDTO.setUserBirth(user.getUserBirth());
-        userDTO.setUserAddress(user.getUserAddress());
-        userDTO.setUserAddressDetail(user.getUserAddressDetail());
-        userDTO.setUserPostal(user.getUserPostal());
-        userDTO.setUserEmail(user.getUserEmail());
-        userDTO.setUserRole(user.getUserRole());
-        Integer result = sqlSession.insert("UserService.setUser", userDTO);        
-        return (result == null) ? 0 : result;
+        try {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUserID(user.getUserID());
+            userDTO.setUserPW(user.getUserPW());
+            userDTO.setUserNickname(user.getUserNickname());
+            userDTO.setUserName(user.getUserName());
+            userDTO.setUserBirth(user.getUserBirth());
+            userDTO.setUserAddress(user.getUserAddress());
+            userDTO.setUserAddressDetail(user.getUserAddressDetail());
+            userDTO.setUserPostal(user.getUserPostal());
+            userDTO.setUserEmail(user.getUserEmail());
+            userDTO.setUserRole(user.getUserRole());
+            Integer result = sqlSession.insert("UserService.setUser", userDTO);
+            return (result == null) ? 0 : result;
+        } catch (Exception e) {
+            return 0;
+        }
     }
 
     @Override
     public boolean deleteUser(int userIdx) throws Exception {
-        int deleteResult = sqlSession.delete("UserService.deleteUser", userIdx);
-        return deleteResult > 0;
+        try {
+            int deleteResult = sqlSession.delete("UserService.deleteUser", userIdx);
+            return deleteResult > 0;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
     public boolean updateUser(UserVO user) throws Exception {
-        int updateResult = sqlSession.update("UserService.updateUser", user);
-        return updateResult > 0;
+        try {
+            int updateResult = sqlSession.update("UserService.updateUser", user);
+            return updateResult > 0;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
     public String findId(String userName, String userEmail) throws Exception {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUserName(userName);
-        userDTO.setUserEmail(userEmail);
-        return sqlSession.selectOne("UserService.findId", userDTO);
+        try {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUserName(userName);
+            userDTO.setUserEmail(userEmail);
+            return sqlSession.selectOne("UserService.findId", userDTO);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @Override
     public boolean findPw(String userName, String userEmail, String userID) throws Exception {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUserName(userName);
-        userDTO.setUserEmail(userEmail);
-        userDTO.setUserID(userID);
-        return sqlSession.selectOne("UserService.findPw", userDTO) != null;
+        try {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUserName(userName);
+            userDTO.setUserEmail(userEmail);
+            userDTO.setUserID(userID);
+            return sqlSession.selectOne("UserService.findPw", userDTO) != null;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
     public boolean changePw(String userName, String userEmail, String userID, String userPW) throws Exception {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUserName(userName);
-        userDTO.setUserEmail(userEmail);
-        userDTO.setUserID(userID);
-        userDTO.setUserPW(userPW);
-        return sqlSession.update("UserService.changePw", userDTO) > 0;
+        try {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUserName(userName);
+            userDTO.setUserEmail(userEmail);
+            userDTO.setUserID(userID);
+            userDTO.setUserPW(userPW);
+            return sqlSession.update("UserService.changePw", userDTO) > 0;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
