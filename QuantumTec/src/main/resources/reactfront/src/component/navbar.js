3550883694edd623431d3129847b2034397d7c1c
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-05-30 20:21:24 +0900

    feat[#7]: 로그인 후 아이콘 클릭 시 기본 정보를 출력 기능 추가

--- QuantumTec/src/main/resources/reactfront/src/component/navbar.js
+++ QuantumTec/src/main/resources/reactfront/src/component/navbar.js
@@ -5,15 +5,27 @@ import '../App.js';
 // import React, { useState, useEffect } from "react";
 import json from './b.json';
 
-export default function Navbar(props) {
-    let userpaidCash=0;
-    let userCash = 0;
-    let userCheck = 8;
-    let truelogin = false;
-    // 로그인 상태일때 유저 이름 받아오기
-    truelogin=props.start;
-    let username= 'test';
-    truelogin=true;
+export default function Navbar() {
+    let truelogin=localStorage.getItem("truelogin");
+
+    let defaultIconAddress = 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png';
+
+    const [userNickname, setUserNickname] = useState('');               // 사용자 닉네임
+    const [userPaidCash, setUserPaidCash] = useState(0);                // 사용자 유료캐시
+    const [userFreeCash, setUserFreeCash] = useState(0);                // 사용자 무료캐시
+    const [userAttendance, setUserAttendance] = useState(0);            // 사용자 출석횟수
+    const [userIconAddress, setUserIconAddress] = useState(defaultIconAddress);    // 사용자 아이콘 주소
+
+    useEffect(() => {
+        // 로그인 상태일때 유저 이름 받아오기
+        if(truelogin){
+            setUserNickname(localStorage.getItem("userNickname") || '');
+            setUserPaidCash(localStorage.getItem("userPaidCash") || 0);
+            setUserFreeCash(localStorage.getItem("userCash") || 0);
+            setUserAttendance(localStorage.getItem("userAttendance") || 0);
+            setUserIconAddress(localStorage.getItem("userIcon") || defaultIconAddress);
+        }
+    }, [truelogin]);
 
     const handleLogInfo = (e) => {
         console.log(e.currentTarget)
@@ -23,43 +35,12 @@ export default function Navbar(props) {
         else
             e.currentTarget.nextSibling.style.display = 'block';
     }
-    // 로그인 상태일때 유저 이름 받아오기
-    const getUsername = () => { 
-        const url = json;
-        axios
-        .get(url)
-        .then(res => username = res.data.username)
-        .catch(username = 'error');
-        
-    };
-    const getMyCash = () => {
-        const url = json;
-        axios
-        .get(url)
-        .then(res => userCash = (res.data.userCash))
-        .catch(userCash = '0');
-    };
 
     // 로고 클릭시 맨 위로 이동
     const logoClick = () => {
         document.location.href = "/";
     };
 
-
-//     const [username, setUsername] = useState("");
-
-//   useEffect(() => {
-//     const url = json;
-//     axios
-//       .get(url)
-//       .then((res) => {
-//         const data = res.data;
-//         setUsername(data.username);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//         }, []);
 
     // 로그인 버튼 클릭시 로그인 페이지로 이동
     const ClickLogin = () => {
@@ -98,8 +79,8 @@ export default function Navbar(props) {
                         </div>
                         <form class="log_info d-flex justify-end absolute top-0 right-0 mr-10 ">
                                 
-                                {getUsername()}
-                                {getMyCash()}
+                                {/*{getUsername()}*/}
+                                {/*{getMyCash()}*/}
                                 {!truelogin && <button class="btn-outline-dark border-2 hover:bg-green-500 hover:text-white rounded-md pr-1" type="button" onClick={ClickLogin} >
                                 <i class="loginbtn me-1"></i>
                                 <span>
@@ -107,33 +88,33 @@ export default function Navbar(props) {
                                 </button>}
                                 {truelogin && <div><a class='flex openLogInfo' href='javascript:void(0)' onClick={handleLogInfo}>
                                     <div class='flex'>
-                                    <div class='mr-1'>{username}</div>
-                                    <div class='mr-5'> : {userCash}</div>
+                                    <div class='mr-1'>{userNickname}</div>
+                                    <div class='mr-5'> : {userFreeCash}</div>
                                     </div>
-                                    <img class='w-8 h-8 rounded-full' src='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png' />
+                                    <img class='w-8 h-8 rounded-full' src={userIconAddress} />
                                 </a>
                                 <div class='LogInfo hidden w-[366px] h-[300px] absolute bg-gray-800 top-[38px] right-0 z-40 overflow-hidden'>
                                             <div class="info_t p-1">
                                                 <ul class='flex mt-4'>
-                                                    <li class="thum"><img class='w-24 h-24 rounded-full' src="https://resource.cyphers.co.kr/ui/img/character/ico_64px_44.png" alt=""/>
+                                                    <li class="thum"><img class='w-24 h-24 rounded-full' src={userIconAddress} alt=""/>
                                                     
                                                     </li>
                                                     <li class='text-gray-400 ml-3'>
                                                         <p class='mb-0 ml-2 text-left font-bold'>
                                                             <strong>
-                                                                {username}
+                                                                {userNickname}
                                                             </strong>
                                                         </p>
-                                                        <p class='mb-0 ml-2 text-left'>출석일수 : <span>{userCheck}일</span></p>
-                                                        <p class='mb-0 ml-2 text-left'>보유재화 : <span>{userCash}원</span></p>
-                                                        <p class='mb-0 ml-2 text-left'>유료재화 : <a href="/mypage"><span class='text-gray-400'>{userpaidCash}원</span></a> <a href="#" class="bg-yellow-400 rounded-md  hover:text-white" id="boxTeraCharge">충전</a></p>
+                                                        <p class='mb-0 ml-2 text-left'>출석일수 : <span>{userAttendance}일</span></p>
+                                                        <p class='mb-0 ml-2 text-left'>보유재화 : <span>{userFreeCash}원</span></p>
+                                                        <p class='mb-0 ml-2 text-left'>유료재화 : <a href="#"><span class='text-gray-400'>{userPaidCash}원</span></a> <a href="#" class="bg-yellow-400 rounded-md  hover:text-white" id="boxTeraCharge">충전</a></p>
                                                     </li>
                                                 </ul>
                                             </div>
                                         <hr class='border-white mb-0'/>
                                         <div class="info_b text-center bg-gray-900">
-                                            <a class='inline-block px-[18px] py-[20px] text-gray-400 text-lg' href="/mypage">내정보관리</a>
-                                            <a class='inline-block px-[18px] py-[20px] text-gray-400 text-lg' href="/mypage">아바타관리</a>
+                                            <a class='inline-block px-[18px] py-[20px] text-gray-400 text-lg' href="#">내정보관리</a>
+                                            <a class='inline-block px-[18px] py-[20px] text-gray-400 text-lg' href="#">아바타관리</a>
                                         </div>
                                         <hr class='border-white mb-0 mt-0'/>
                                         <div class='text-center bg-gray-900'>
