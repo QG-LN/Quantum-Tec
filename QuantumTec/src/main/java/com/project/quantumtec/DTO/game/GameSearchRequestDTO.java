767d0b777fbf815e6071bcc15df0fb00b31f79fc
YeChanKang <yechan.dev@gmail.com>
2023-06-02 11:01:32 +0900

    feat: [#11] GameSearchRequestDTO에서 페이지 스크롤 위치를 파악하기 위한 startNum, endNum 추가

--- QuantumTec/src/main/java/com/project/quantumtec/DTO/game/GameSearchRequestDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/game/GameSearchRequestDTO.java
@@ -7,4 +7,6 @@ public class GameSearchRequestDTO {
     private String gameName; // 게임 이름
     private String[] gameGenre; // 게임 장르 (여러개 선택될 수 있음)
     private int gamePrice; // 게임 가격
+    private int startNum; // 시작 번호
+    private int endNum; // 끝 번호
 }
