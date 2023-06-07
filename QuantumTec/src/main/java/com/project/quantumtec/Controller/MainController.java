07f9a2cbf496b96b1c92d9abe8f73ad9559a978b
YeChanKang <yechan.dev@gmail.com>
2023-06-08 03:48:34 +0900

    feat: [#50] 게시판 기능을 위해 MainController에 게시물 리스트를 불러오는 메소드 추가, 데이터 전달에 사용될 BoardRequestDTO 생성, 검색 결과를 담을 BoardResponseDTO 생성.

--- QuantumTec/src/main/java/com/project/quantumtec/Controller/MainController.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Controller/MainController.java
@@ -1,9 +1,25 @@
 package com.project.quantumtec.Controller;
 
+import com.project.quantumtec.DTO.board.BoardRequestDTO;
+import com.project.quantumtec.DTO.board.BoardResponseDTO;
+import com.project.quantumtec.Service.board.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/")
 public class MainController
 {
-    
+    @Autowired
+    private BoardService boardService;
+    @PostMapping("/board")
+    public List<BoardResponseDTO> getPostSearchList(@RequestBody BoardRequestDTO board) throws Exception {
+        return boardService.getPostSearchList(board);
+    }
+
 }
