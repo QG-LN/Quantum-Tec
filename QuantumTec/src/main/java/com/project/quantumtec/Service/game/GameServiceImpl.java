34bea8328a39beed6f7ee9368afe57866922806a
YeChanKang <yechan.dev@gmail.com>
2023-06-08 01:24:33 +0900

    feat: [#11] 프론트에서 페이지 번호를 받아오면, 그 번호를 기준으로 시작 인덱스 값을 계산해 넣고, 표시할 아이템 갯수를 넣어서 DB로부터 값을 가져오는 방식으로 수정되었음

--- QuantumTec/src/main/java/com/project/quantumtec/Service/game/GameServiceImpl.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Service/game/GameServiceImpl.java
@@ -16,6 +16,10 @@ public class GameServiceImpl implements GameService{
     private GameDAO gameDAO;
     @Override
     public List<GameSearchResponseDTO> getGameSearchList(GameSearchRequestDTO game){
+        // 페이지 처리
+        int itemNum = 16; // 한페이지 아이템 갯수
+        game.setStartIndex((game.getPageNum() - 1) * itemNum); // DB에서 시작하는 인덱스 값을 받기로 함 (시작번호)
+        game.setEndIndex(itemNum); // DB에서 한페이지 아이템 갯수를 받기로 함 (표시할 아이템 갯수)
         return gameDAO.getGameSearchList(game);
     }
 
