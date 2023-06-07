f278e66c52ed252de7f7bdca8c4b150c7239eff3
YeChanKang <yechan.dev@gmail.com>
2023-06-08 00:49:20 +0900

    feat: [#11] 변수 이름 오타 cart -> cat

--- QuantumTec/src/main/java/com/project/quantumtec/DTO/game/GameSearchResponseDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/game/GameSearchResponseDTO.java
@@ -7,6 +7,6 @@ public class GameSearchResponseDTO {
     private int gameIndex; // 게임 아이디
     private String gameName; // 게임 이름
     private String gamePrice; // 게임 가격
-    private String gameCartegoryName; // 게임 장르
+    private String gameCategoryName; // 게임 장르
     private String gameImage; // 게임 이미지
 }
