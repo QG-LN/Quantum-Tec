c69d59db531a8b40204d66c92436667f0efd9e9a
YeChanKang <yechan.dev@gmail.com>
2023-06-08 01:24:33 +0900

    feat: [#11] 프론트에서 페이지 번호를 받아오면, 그 번호를 기준으로 시작 인덱스 값을 계산해 넣고, 표시할 아이템 갯수를 넣어서 DB로부터 값을 가져오는 방식으로 수정되었음

--- QuantumTec/src/main/java/com/project/quantumtec/DTO/game/GameSearchRequestDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/game/GameSearchRequestDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class GameSearchRequestDTO {
+    private int pageNum; // 페이지 번호
     private int startIndex; // 시작 인덱스
     private int endIndex; // 끝 인덱스
     private String gameName; // 게임 이름
