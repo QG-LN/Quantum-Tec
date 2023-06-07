c3ecb2ddc8efe8284af5e49ae884319d77b1dc26
YeChanKang <yechan.dev@gmail.com>
2023-06-07 22:07:29 +0900

    feat: [#11] 메인페이지에서 제공해야 하는 로그인 이후 받아올 데이터들을 담는 DTO 수정

--- QuantumTec/src/main/java/com/project/quantumtec/DTO/user/LoginResponseDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/user/LoginResponseDTO.java
@@ -5,5 +5,8 @@ import lombok.Data;
 @Data
 public class LoginResponseDTO {
     private String userNickname; // 사용자 닉네임
-    private int userCash; // 사용자 재화
+    private String userAttendanceDays; // 사용자 출석일 수
+    private int userCash; // 사용자 유료 재화
+    private int userFreeCash; // 사용자 무료 재화
+    //private String userAvatar; // 사용자 아바타 (임시)
 }
