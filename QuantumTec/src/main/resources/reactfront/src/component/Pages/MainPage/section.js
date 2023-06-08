9554149ceabe8c9cd2b9eef9ebb7ba2340c9bb0e
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-06-09 04:09:39 +0900

    feat :[#11] 스크롤 / 카테고리 선택시에 따른 게임리스트 출력 [front-back]연결

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/MainPage/section.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/MainPage/section.js
@@ -4,76 +4,98 @@ import Gamelist from './gamelist.js';
 import axios from 'axios';
 import Gamepage from '../GamePage/gamepage.js';
 import {useInView} from 'react-intersection-observer';
-
+import {axiosRequest} from '../../../module/networkUtils';
 
 export default function Section() {
-    const [gamelist, setgameList] = useState(["게임이름", "수학게임"]);
-    const [inputCate, setInputCate] = useState("#전체");
-    const [items, setItems] = useState([])
-    const [page, setPage] = useState(1)
-    const [loading, setLoading] = useState(false)
+    const [gamelist, setGamelist] = useState([]);           // 서버에서 받아온 게임 리스트
+    const [inputCate, setInputCate] = useState("전체");        // 선택한 카테고리
+    const [items, setItems] = useState([])                  // 보여줄 게임 리스트
+    const [page, setPage] = useState(1)                     // 현재 페이지
 
-    const [ref, inView] = useInView()
+    const [ref, inView] = useInView()                                 // 스크롤이 끝에 도달했는지 여부
+    const [loading, setLoading] = useState(false)           // 로딩중인지 여부
 
-    const maxContentLength = 8;
+    const defaultImage = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"; // 이미지가 없을 경우 기본 이미지
 
     const handleInputCate = (e) => {
         setInputCate(e.target.value)
     }
 
 
-    useEffect(() => {
-        console.log("useEffect")
-        setLoading(true)
-        axios.get("api/games", {
-            params: {
-                id         : '0',
-                cate       : '',
-                name       : '',
-                img        : '',
-                description: '',
-                link       : ''
-            }
-        })
-            .then(response => setItems(prevItems => {
-                prevItems.concat(response.data);
-            }))
+    // 게임 목록을 받아오는 함수
+    function getGameList(){
+        const path = 'http://localhost:9090/search';
+        let category = inputCate === "전체" ? "" : inputCate;
+        const body = {
+            pageNum         : page,
+            gameName        : "",
+            gameCategoryName: category,
+            gamePrice       : 0
+        };
+        axiosRequest(path, body, 'post', 'list')
+            .then(res => {
+                setGamelist(res);            // 서버에서 받아온 게임 목록 저장
+                if (res.length !== 0) {      // 받아온 게임 목록이 있을 경우
+                    const temp = [];        // 새로 받아온 게임 목록을 임시로 저장할 배열
+                    /*
+                    * 새로 받아온 게임 목록을 모두 temp에 저장한 뒤 items에 추가
+                    * */
+                    for (let i = 0; i < res.length; i++) {
+                        if (res[i] !== null) {
+                            if (res[i].gameImage == null) res[i].gameImage = defaultImage;
+                            const newItem = {
+                                id         : res[i].gameIndex,
+                                cate       : res[i].gameCategoryName,
+                                name       : res[i].gameName,
+                                img        : res[i].gameImage,
+                                description: "수학게임입니다.",
+                                link       : "/gamepage"                // 임시로 동일하게 게임 페이지를 설정
+                            };
+                            temp.push(newItem);
+                        }
+
+                    }
+                    // 기존 게임 목록에 새로 받아온 게임 목록 추가
+                    setItems(prevItems => prevItems.concat(temp));
+
+                }
+            })
             .catch(error => {
                 console.error(error);
-            })
-        const temp = [];
-        for (let i = 0 + (maxContentLength * (page - 1)); i < maxContentLength * page; i++) {
-            const newItem = {
-                id         : 0,
-                cate       : 1,
-                name       : "수학게임" + i,
-                img        : "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-                description: "수학게임입니다.",
-                link       : "/gamepage"
-            };
-            temp.push(newItem);
-        }
-        setItems(items.concat(temp))
-        setLoading(false)
+            });
+    }
+
+    // 페이지가 바뀌었을 때마다 게임 목록을 새로 받아옴
+    useEffect(() => {
+        setLoading(true);
+
+        getGameList();
+
+        setLoading(false);
     }, [page]);
 
     useEffect(() => {
         // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
         if (inView && !loading) {
-            setPage(page + 1)
+            if(gamelist.length !==0)  setPage(page + 1);
+            console.log('0');
         }
     }, [inView, loading])
 
-
-    // 더블클릭 문제 해결을 위한 id값으로 checked 설정
+    // 카테고리가 바뀌었을 때마다 게임 목록을 새로 받아옴
     useEffect(() => {
-        // let checkresult = document.getElementById("cate");
-        // checkresult.checked = true;
-    }, []);
+        if(page !== 1) {            // 페이지가 1이 아닐 경우 페이지를 1로 초기화하여 page useEffect를 실행
+            setPage(1);
+        }else{                      // 페이지가 1일 경우 기존 게임 목록을 삭제하고 새로 받아옴
+            getGameList();
+        }
+        setItems([]);
+    },[inputCate]);
+
+
 
     const Clickcate = (e) => {
         let index = e.target;
-        let index2 = index.text;
     }
     const Clicksearch = () => {
     }
@@ -98,31 +120,31 @@ export default function Section() {
                     <fieldset class='radioButtonStyle'>
                         <legend class='absolute overflow-hidden h-1 w-1 m-[-1px]'></legend>
                         <label className='radioStyle hover:cursor-pointer'>
-                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='0' checked={inputCate === '0'}/>
+                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='전체' checked={inputCate === '전체'}/>
                             <span onClick={Clickcate}>#전체</span>
                         </label>
                         <label className='radioStyle hover:cursor-pointer'>
-                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='1' checked={inputCate === '1'}/>
+                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='수학' checked={inputCate === '수학'}/>
                             <span onClick={Clickcate}>#수학</span>
                         </label>
                         <label className='radioStyle hover:cursor-pointer'>
-                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='2' checked={inputCate === '2'}/>
+                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='국어' checked={inputCate === '국어'}/>
                             <span onClick={Clickcate}>#국어</span>
                         </label>
                         <label className='radioStyle hover:cursor-pointer'>
-                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='3' checked={inputCate === '3'}/>
+                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='과학' checked={inputCate === '과학'}/>
                             <span onClick={Clickcate}>#과학</span>
                         </label>
                         <label className='radioStyle hover:cursor-pointer'>
-                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='4' checked={inputCate === '4'}/>
+                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='영어' checked={inputCate === '영어'}/>
                             <span onClick={Clickcate}>#영어</span>
                         </label>
                         <label className='radioStyle hover:cursor-pointer'>
-                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='5' checked={inputCate === '5'}/>
+                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='사회' checked={inputCate === '사회'}/>
                             <span onClick={Clickcate}>#사회</span>
                         </label>
                         <label className='radioStyle hover:cursor-pointer'>
-                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='6' checked={inputCate === '6'}/>
+                            <input type="radio" name='cate' id='cate' onChange={handleInputCate} value='일본어' checked={inputCate === '일본어'}/>
                             <span onClick={Clickcate}>#일본어</span>
                         </label>
                     </fieldset>
