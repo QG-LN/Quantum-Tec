b1d0b6c3b6853d87b8502a88a2a0fdb0588bb806
YeChanKang <yechan.dev@gmail.com>
2023-06-08 13:41:16 +0900

    feat: [#50] 메인 컨트롤러를 롤백(컨트롤러 분리로 인한)

--- QuantumTec/src/main/java/com/project/quantumtec/Controller/MainController.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Controller/MainController.java
@@ -1,11 +1,5 @@
 package com.project.quantumtec.Controller;
 
-import com.project.quantumtec.DTO.board.BoardRequestDTO;
-import com.project.quantumtec.DTO.board.BoardResponseDTO;
-import com.project.quantumtec.Service.board.BoardService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +9,6 @@ import java.util.List;
 @RequestMapping("/")
 public class MainController
 {
-    @Autowired
-    private BoardService boardService;
-    @PostMapping("/board")
-    public List<BoardResponseDTO> getPostSearchList(@RequestBody BoardRequestDTO board) throws Exception {
-        return boardService.getPostSearchList(board);
-    }
+
 
 }
