e8262b2bcd4df3bb83e108b44535089d5cc2310b
MayoneJY <mayone6063@kakao.com>
2023-06-08 15:24:54 +0900

    feat: [#57] 첫 페이지에 모든 카테고리 아이템 나오도록 추가

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/AvatarShopPage/avatarMain.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/AvatarShopPage/avatarMain.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "./css.scss";
 export default function AvatarMain() {
+    const [avatarCategory, setAvatarCategory] = useState([]); // 카테고리 목록
     const [hatList, setHatList] = useState([]);
     useEffect(() => {
         axios.get('http://localhost:8080/api/avatar/hat')
@@ -11,14 +12,28 @@ export default function AvatarMain() {
             .catch((error) => {
                 console.log(error);
             });
-        const tempArray = [];
+        let tempArray = [];
         for(let i = 0; i < 5; i++) {
             tempArray.push({ id: i, name: '모자' });
         }
-        setHatList(hatList.concat(tempArray))
+        setHatList(hatList.concat(tempArray));
+        tempArray = []
+        tempArray.push({ id: 1, name: '모자 아이템' });
+        tempArray.push({ id: 2, name: '악세서리 아이템' });
+        tempArray.push({ id: 3, name: '머리카락 아이템' });
+        tempArray.push({ id: 4, name: '이너 아이템' });
+        tempArray.push({ id: 5, name: '아웃터 아이템' });
+        tempArray.push({ id: 6, name: '바지 아이템' });
+        tempArray.push({ id: 7, name: '치마 아이템' });
+        tempArray.push({ id: 8, name: '원피스 아이템' });
+        tempArray.push({ id: 9, name: '신발 아이템' });
+        tempArray.push({ id: 10, name: '구두 아이템' });
+        tempArray.push({ id: 11, name: '배경 아이템' });
+        tempArray.push({ id: 12, name: '가구 아이템' });
+        setAvatarCategory(avatarCategory.concat(tempArray))
     }, []);
     return (
-        <div className='bg-gray-200 h-[100%]'>
+        <div className='bg-gray-200'>
             <div className='p-2 pt-5 me-0 user-wrap'>
                 <img className="rounded mx-auto d-block w-[60%] user-image" src='https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?resize=476%2C280&ssl=1' alt="..." />
                 <div className='user-text text-white'>
@@ -26,29 +41,34 @@ export default function AvatarMain() {
                     <h5>포인트 상점 아이템으로 자신만의 모습을 표현해보세요</h5>
                 </div>
             </div>
-            <div className='d-flex align-items-center'>
-                <h5 className='w-[18%]'>모든 모자 아이템</h5>
-                <button type="button" class="btn btn-secondary btn-sm">Secondary</button>
-                <hr className='flex-fill mx-3'/>
-            </div>
-            <div className='ms-4 mt-4 d-flex flex-wrap align-items-center'>
-                {hatList.map((item) => (
-                    <div class="card w-[18.5%] ms-2" aria-hidden="true">
-                        <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" class="card-img-top" alt="..."/>
-                        <div class="text-start m-3">
-                            <h5 class="card-title placeholder-glow">
-                            <div class="placeholder col-5"></div>
-                            </h5>
-                            <h6 class="card-text placeholder-glow">
-                                <div class="placeholder col-7"></div>
-                            </h6>
-                            <h6 class="card-text placeholder-glow text-end">
-                                <div class="placeholder col-5"></div>
-                            </h6>
-                        </div>
+            {avatarCategory.map((item) => (
+                <div className='mt-2 mb-5'>
+                    <div className='d-flex align-items-center'>
+                        <h5 className={item.name.length > 6 ? 'w-[20%]' : 'w-[18%]'}>모든 {item.name}</h5>
+                        <button type="button" class="btn btn-secondary btn-sm">모두 보기 (22)</button>
+                        <hr className='flex-fill mx-3'/>
                     </div>
-                ))}
-            </div>
+                    <div className='ms-4 mt-4 d-flex flex-wrap align-items-center'>
+                        {hatList.map((item) => (
+                            <div class="card w-[18.5%] ms-2" aria-hidden="true">
+                                <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" class="card-img-top" alt="..."/>
+                                <div class="text-start m-3">
+                                    <h5 class="card-title placeholder-glow">
+                                    <div class="placeholder col-5"></div>
+                                    </h5>
+                                    <h6 class="card-text placeholder-glow">
+                                        <div class="placeholder col-7"></div>
+                                    </h6>
+                                    <h6 class="card-text placeholder-glow text-end">
+                                        <div class="placeholder col-5"></div>
+                                    </h6>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
+            
             
         </div>
     );
