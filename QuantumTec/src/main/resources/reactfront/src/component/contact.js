6e6241e44ff304053d43871faf964a8664ef0456
Junsoi <83137169+saetu0422@users.noreply.github.com>
2023-06-08 14:16:35 +0900

    [feat] #60 nav바 수정
    왼쪽 게시판쪽 아래줄 삭제
    로그인 버튼 디자인 수정
    상단바 고정

--- QuantumTec/src/main/resources/reactfront/src/component/contact.js
+++ QuantumTec/src/main/resources/reactfront/src/component/contact.js
@@ -13,15 +13,16 @@ export default function Contact() {
                 <div className="flex flex-wrap justify-center">
                     {/*커뮤니티게시판, 튜터링게시판 이동 버튼 list*/}
                         <h5 className="text-2xl font-semibold">커뮤니티게시판</h5>
+                        <hr className="w-[100%] mt-[1%] border-0 bg-black opacity-100 h-[2px]"/>
 
                         <nav className="flex flex-col list-none">
                             <li className="nav-item">
-                                <a className="px-3 py-2 flex items-center text-xl uppercase font-bold leading-snug text-black hover:opacity-75" href="/board">
+                                <a className="px-3 py-2 flex items-center text-xl uppercase font-bold leading-snug text-black hover:opacity-75 no-underline" href="/board">
                                     <i className="fab fa-facebook-square text-xs leading-lg text-black opacity-75"></i><span className="ml-2">자유게시판</span>
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className="px-3 py-2 flex items-center text-xl uppercase font-bold leading-snug text-black hover:opacity-75" href="/board">
+                                <a className="px-3 py-2 flex items-center text-xl uppercase font-bold leading-snug text-black hover:opacity-75 no-underline" href="/board">
                                     <i className="fab fa-facebook-square text-xs leading-lg text-black opacity-75"></i><span className="ml-2">질문게시판</span>
                                 </a>
                             </li>
