b5e9f7cf7ad5c0dfe3e3b732a589b63a7de4fcd4
YeChanKang <yechan.dev@gmail.com>
2023-06-08 14:45:01 +0900

    feat: [#50] 게시글 작성, 수정, 삭제, 좋아요, 싫어요 에 해당하는 RequestDTO객체들 생성

---
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/board/BoardModifyRequestDTO.java
@@ -0,0 +1,9 @@
+package com.project.quantumtec.DTO.board;
+
+public class BoardModifyRequestDTO {
+    private int boardIndex; // 게시판 번호
+    private int postIndex; // 게시글 번호
+    private int userIndex; // 유저 번호
+    private String title; // 제목
+    private String content; // 내용
+}
---
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/board/BoardUpvoteRequestDTO.java
@@ -0,0 +1,7 @@
+package com.project.quantumtec.DTO.board;
+
+public class BoardUpvoteRequestDTO {
+
+    private int postIndex; // 게시글 번호
+    private int userIndex; // 유저 번호
+}
