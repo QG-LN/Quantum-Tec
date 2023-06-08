e8a16dc72732e17302b094034872c00770449278
YeChanKang <yechan.dev@gmail.com>
2023-06-08 14:44:06 +0900

    feat: [#50] BoardService에 게시글 작성, 수정, 삭제, 추천, 비추천 메소드 추가

--- QuantumTec/src/main/java/com/project/quantumtec/Service/board/BoardService.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Service/board/BoardService.java
@@ -1,28 +1,29 @@
 package com.project.quantumtec.Service.board;
 
-import com.project.quantumtec.DTO.board.BoardListRequestDTO;
-import com.project.quantumtec.DTO.board.BoardListResponseDTO;
-import com.project.quantumtec.DTO.board.BoardViewRequestDTO;
-import com.project.quantumtec.DTO.board.BoardViewResponseDTO;
+import com.project.quantumtec.DTO.board.*;
 
 
 import java.util.List;
 
 public interface BoardService {
     // 게시물 리스트 불러오기 (검색 포함)
-    public List<BoardListResponseDTO> getPostSearchList(BoardListRequestDTO board);
+    public List<BoardListResponseDTO> getPostSearchList(BoardListRequestDTO request);
 
     // 게시물 조회
-    public BoardViewResponseDTO getPost(BoardViewRequestDTO board);
+    public BoardViewResponseDTO getPost(BoardViewRequestDTO request);
 
     // 게시물 작성
+    public boolean writePost(BoardWriteRequestDTO request);
 
     // 게시물 수정
+    public boolean modifyPost(BoardModifyRequestDTO request);
 
     // 게시물 삭제
+    public boolean deletePost(BoardDeleteRequestDTO request);
 
     // 게시물 좋아요
+    public boolean upvotePost(BoardUpvoteRequestDTO request);
 
     // 게시물 싫어요
-
+    public boolean downvotePost(BoardDownvoteRequestDTO request);
 }
