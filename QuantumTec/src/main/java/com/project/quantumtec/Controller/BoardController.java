6a354357caf20ff35a1fd465cb607eb9157e5891
YeChanKang <yechan.dev@gmail.com>
2023-06-08 14:43:05 +0900

    feat: [#50] BoardController에 게시글 작성, 수정, 삭제, 추천, 비추천 메소드 추가

--- QuantumTec/src/main/java/com/project/quantumtec/Controller/BoardController.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Controller/BoardController.java
@@ -1,10 +1,7 @@
 package com.project.quantumtec.Controller;
 
 
-import com.project.quantumtec.DTO.board.BoardListRequestDTO;
-import com.project.quantumtec.DTO.board.BoardListResponseDTO;
-import com.project.quantumtec.DTO.board.BoardViewRequestDTO;
-import com.project.quantumtec.DTO.board.BoardViewResponseDTO;
+import com.project.quantumtec.DTO.board.*;
 import com.project.quantumtec.Service.board.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,13 +18,59 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
+    // 게시물 리스트 조회
     @PostMapping("/list")
-    public List<BoardListResponseDTO> getPostSearchList(@RequestBody BoardListRequestDTO board) throws Exception {
-        return boardService.getPostSearchList(board);
+    public List<BoardListResponseDTO> getPostSearchList(@RequestBody BoardListRequestDTO request) throws Exception {
+        return boardService.getPostSearchList(request);
     }
 
+    // 게시물 조회
     @PostMapping("/view")
-    public BoardViewResponseDTO getPost(@RequestBody BoardViewRequestDTO board) throws Exception {
-        return boardService.getPost(board);
+    public BoardViewResponseDTO getPost(@RequestBody BoardViewRequestDTO request) throws Exception {
+        return boardService.getPost(request);
     }
+
+    // 게시물 작성
+    @PostMapping("/write")
+    public boolean writePost(@RequestBody BoardWriteRequestDTO request) throws Exception {
+        return boardService.writePost(request);
+    }
+
+    // 게시물 수정
+    @PostMapping("/modify")
+    public boolean modifyPost(@RequestBody BoardModifyRequestDTO request) throws Exception {
+        return boardService.modifyPost(request);
+    }
+
+    // 게시물 삭제
+    @PostMapping("/delete")
+    public boolean deletePost(@RequestBody BoardDeleteRequestDTO request) throws Exception {
+        return boardService.deletePost(request);
+    }
+
+    // 게시물 추천
+    @PostMapping("/upvote")
+    public boolean upvotePost(@RequestBody BoardUpvoteRequestDTO request) throws Exception {
+        return boardService.upvotePost(request);
+    }
+
+    // 게시물 비추천
+    @PostMapping("/downvote")
+    public boolean downvotePost(@RequestBody BoardDownvoteRequestDTO request) throws Exception {
+        return boardService.downvotePost(request);
+    }
+
+//    // 다음 게시글
+//    @PostMapping("/next")
+//    public BoardViewResponseDTO getNextPost(@RequestBody BoardViewRequestDTO request) throws Exception {
+//        return boardService.getPost(request);
+//    }
+//
+//    // 이전 게시글
+//    @PostMapping("/prev")
+//    public BoardViewResponseDTO getPrevPost(@RequestBody BoardViewRequestDTO request) throws Exception {
+//        return boardService.getPost(request);
+//    }
+
 }
+
