b8013e422f71d36ec67728ace2228b8684dc9961
YeChanKang <yechan.dev@gmail.com>
2023-06-08 16:53:34 +0900

    feat: [#50] 댓글 작성, 수정, 삭제 요청을 담을 DTO들 생성

---
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/board/BoardCommentDeleteRequestDTO.java
@@ -0,0 +1,7 @@
+package com.project.quantumtec.DTO.board;
+
+public class BoardCommentDeleteRequestDTO {
+    private int comment_index; // 댓글 번호
+    private int post_index; // 게시글 번호
+    private int user_index; // 유저 번호
+}
---
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/board/BoardCommentModifyRequestDTO.java
@@ -0,0 +1,8 @@
+package com.project.quantumtec.DTO.board;
+
+public class BoardCommentModifyRequestDTO {
+    private int post_index; // 게시글 번호
+    private int comment_index; // 댓글 번호
+    private int user_index; // 유저 번호
+    private String comment_content; // 댓글 내용
+}
