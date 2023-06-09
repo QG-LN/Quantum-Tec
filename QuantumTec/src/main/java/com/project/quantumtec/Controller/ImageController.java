d42475419d9844af1ab79afabdaf4cabb5b22632
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-06-10 04:23:33 +0900

    feat: [#13] 기본 내용 및 메인이미지를 화면에 출력 [front-back]연결

--- QuantumTec/src/main/java/com/project/quantumtec/Controller/ImageController.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Controller/ImageController.java
@@ -2,10 +2,11 @@ package com.project.quantumtec.Controller;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * PackageName : com.project.quantumtec.Controller
@@ -15,10 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
  * Description :
  */
 @RestController
-@RequestMapping("/images")
+@RequestMapping("/image")
 public class ImageController {
-    @GetMapping("/{imageName}")
-    public Resource getImage(@PathVariable String imageName) {
-        return new ClassPathResource("static/images/" + imageName);
+
+    /**
+     * 경로를 받아서 게임 이미지 1개를 반환하는 메소드
+     * @param imagePath 이미지 경로 (ex: game_1_1.png -> game/1/1.png)를 의미
+     * */
+    @GetMapping("/game/{path:.+}")
+    public Resource getImage(@PathVariable(value = "path") String imagePath) {
+        try{
+            imagePath = imagePath.replace("_", "/");            // _을 /로 변경
+            String path = "static/images/"+ imagePath;
+            return new ClassPathResource(path);
+        }catch (Exception e){
+            System.out.println("이미지가 없습니다.");
+            System.out.println(e.getMessage());
+            return new ClassPathResource("static/images/test.png");
+        }
     }
+
 }
