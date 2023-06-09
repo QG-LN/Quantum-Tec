d701acceea3b47ea139ecd35a5189143e66e50eb
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-06-10 05:39:29 +0900

    feat: [#13] 이미지를 여러개 가져오기위한 기능 추가

--- QuantumTec/src/main/java/com/project/quantumtec/Controller/ImageController.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Controller/ImageController.java
@@ -1,5 +1,6 @@
 package com.project.quantumtec.Controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.*;
@@ -36,4 +37,37 @@ public class ImageController {
         }
     }
 
+    /**
+     * 이미지가 존재하는 디렉토리 경로를 받아 해당 디렉토리 하위에 있는 파일 리스트를 반환
+     * @param imagePath 이미지 경로 (ex: game_1_1.png -> game/1/1.png)를 의미
+     * */
+    @GetMapping("/game/list/{path:.+}")
+    public String  getImageList(@PathVariable(value = "path") String imagePath){
+        try {
+            List<String> imagePaths = new ArrayList<>();
+            imagePath = imagePath.replace("_", "/");            // _을 /로 변경
+            String path = "static/images/" + imagePath;
+            File dir = new File("src/main/resources/" + path);
+            File[] fileList = dir.listFiles();
+            if (fileList != null) {
+                for (File file : fileList) {
+                    if (file.isFile()) {
+                        imagePaths.add(imagePath + "_" +file.getName());
+                        System.out.println(imagePath + "_" +file.getName());
+                    }
+                }
+            }
+
+
+            // 이미지 파일 경로 배열을 JSON으로 변환
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(imagePaths);
+
+            return json;
+        } catch (Exception e) {
+            System.out.println("이미지가 없습니다.");
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 }
