4b6c364823483277f5dfc25f3475f6ca1e9d8216
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-06-10 00:10:53 +0900

    feat :[#13] 게임 index와 게임 이름을 통해 게임정보를 반환하는 기능 추가

---
+++ QuantumTec/src/main/java/com/project/quantumtec/Controller/GameController.java
@@ -0,0 +1,38 @@
+package com.project.quantumtec.Controller;
+
+import com.project.quantumtec.DTO.game.GameDTO;
+import com.project.quantumtec.DTO.game.GameSearchDTO;
+import com.project.quantumtec.Service.game.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * PackageName : com.project.quantumtec.Controller
+ * FileName : GameController
+ * Author : Argonaut
+ * Date : 2023-06-09
+ * Description :
+ */
+
+@RestController
+@RequestMapping("/game")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+    // 게임 ID를 받아서 게임 정보 가져오는 메소드
+    @GetMapping("/info")
+    public GameDTO getGameInfo(@RequestParam(value = "id") int gameID, @RequestParam( value = "name") String gameName){
+        gameName = gameName.replace("_", " ");  // 게임 이름에 _가 포함되어 있으면 공백으로 변경
+
+        // DTO에 게임 ID와 게임 이름을 세팅
+        GameSearchDTO gameSearchDTO = new GameSearchDTO();
+        gameSearchDTO.setGameIndex(gameID);
+        gameSearchDTO.setGameName(gameName);
+
+        // 게임 id와 게임 이름을 받아서 게임 정보를 가져옴
+        return gameService.getGameInfo(gameSearchDTO);
+    }
+
+
+}
