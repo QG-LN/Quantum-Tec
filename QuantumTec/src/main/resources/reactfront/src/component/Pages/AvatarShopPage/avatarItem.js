e6f4d7c287a026126c74b63468da810118add6db
MayoneJY <mayone6063@kakao.com>
2023-06-10 16:20:58 +0900

    feat: [#57] 아이템 클릭시 구매 창 기능 추가

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/AvatarShopPage/avatarItem.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/AvatarShopPage/avatarItem.js
@@ -1,24 +1,71 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 export default function AvatarItem(props) {
+    //modal
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     useEffect(() => {
         // console.log(props);
     }, []);
     return (
         <div class="card w-[18.5%] ms-2 placeholder-glow mb-4" aria-hidden="true">
-            <div class="placeholder ratio ratio-1x1 rounded-top"></div>
-            {/* <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" class="card-img-top" alt="..."/> */}
-            <div class="text-start m-3">
-                <h5 class="card-title placeholder-glow">
-                    {props.item.name}
-                {/* <div class="placeholder col-5"></div> */}
-                </h5>
-                <h6 class="card-text placeholder-glow">
-                    <div class="placeholder col-7"></div>
-                </h6>
-                <h6 class="card-text placeholder-glow text-end">
-                    <div class="placeholder col-5"></div>
-                </h6>
+            <Modal show={show} onHide={handleClose} centered={true}>
+                <Modal.Header>
+                    <Modal.Title>구매</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className='row justify-content-between'>
+                        <div className='placeholder-glow col-5 ms-3'>
+                            <div class="placeholder ratio ratio-1x1 rounded"></div>
+                        </div>
+                        <div className='col-6'>
+                            {props.item.name}
+                            <h5 class="card-text placeholder-glow">
+                                <div class="placeholder col-5"></div>
+                            </h5>
+                            <h5 class="card-text placeholder-glow">
+                                <div class="placeholder col-8"></div>
+                            </h5>
+                            <h5 class="card-text placeholder-glow">
+                                <div class="placeholder col-2"></div>
+                                <div class="placeholder ms-2 col-5"></div>
+                            </h5>
+                            <br/>
+                            <br/>
+                            <h5 class="card-text placeholder-glow">
+                                <div class="placeholder col-4"></div>
+                            </h5>
+                        </div>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button className="btn_close" variant="success" onClick={handleClose}>
+                        구매
+                    </Button>
+                    <Button className="btn_close" variant="secondary" onClick={handleClose}>
+                        닫기
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            <div onClick={handleShow}>
+                <div class="placeholder ratio ratio-1x1 rounded-top"></div>
+                {/* <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" class="card-img-top" alt="..."/> */}
+                <div class="text-start m-3">
+                    <h5 class="card-title placeholder-glow">
+                        {props.item.name}
+                    {/* <div class="placeholder col-5"></div> */}
+                    </h5>
+                    <h6 class="card-text placeholder-glow">
+                        <div class="placeholder col-7"></div>
+                    </h6>
+                    <h6 class="card-text placeholder-glow text-end">
+                        <div class="placeholder col-5"></div>
+                    </h6>
+                </div>
             </div>
         </div>
     );
