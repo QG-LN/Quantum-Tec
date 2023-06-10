94755a32b6198c71fc8fb71ca12806e9a791bb7b
MayoneJY <mayone6063@kakao.com>
2023-06-10 13:44:44 +0900

    refactor: [#57] avatarItem 컴포넌트 추가

---
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/AvatarShopPage/avatarItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function AvatarItem() {
+    return (
+        <div class="card w-[18.5%] ms-2 placeholder-glow mb-4" aria-hidden="true">
+            <div class="placeholder ratio ratio-1x1 rounded-top"></div>
+            {/* <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" class="card-img-top" alt="..."/> */}
+            <div class="text-start m-3">
+                <h5 class="card-title placeholder-glow">
+                <div class="placeholder col-5"></div>
+                </h5>
+                <h6 class="card-text placeholder-glow">
+                    <div class="placeholder col-7"></div>
+                </h6>
+                <h6 class="card-text placeholder-glow text-end">
+                    <div class="placeholder col-5"></div>
+                </h6>
+            </div>
+        </div>
+    );
+}
+
