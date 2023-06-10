514afada99009c6f1ffef824e117b3d56b698652
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-06-10 19:23:39 +0900

    feat: [#13] 이미지를 여러개 가져오기위한 기능 추가

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/GamePage/gpimage.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/GamePage/gpimage.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 
 export default function GameImage(props) {
     //props로 이미지 배열을 가져옴
-    const images = props.img;
-    // console.log(images)
-    
-    //큰 이미지 화면 초기값 설정
-    const [selectImg, setSelectImg] = useState(images[0].url);
+    const imgList = props.imgList;
+    const imgPath = props.imgPath;
 
+    //
+
+    //큰 이미지 화면 초기값 설정
+    const [selectImg, setSelectImg] = useState('');
 
     //이미지 갯수확인
     const handleInputImg = (e) => {
         console.log(e.target.value);
     }
-    
 
 
     //이미지를 클릭할시
@@ -33,20 +33,21 @@ export default function GameImage(props) {
 
     return (
         <div>
-            <img className='w-[720px] h-[405px]' src={selectImg} alt='게임이미지' />
+            <img className='w-[720px] h-[405px]' src={selectImg} alt='게임이미지'/>
             <div class='overflow-x-scroll w-[720px] mt-4'>
                 <div className="image-slider flex">
-                <fieldset class='imgButtonStyle flex'>
-                            <legend class='absolute overflow-hidden h-1 w-1 m-[-1px] '></legend>
-                    {images.map((image, index) => (
-                                    <label className='hover:cursor-pointer w-[160px] h-[90px]'>
-                                        <input type="radio" class='hidden' name='subimg' id='subimg' onChange={handleInputImg} value={index}/><img class='max-w-none w-[160px] h-[90px]'src={image.url} onClick={OnClickimg}></img>
-                                    </label>
-
-                    ))}
-                </fieldset>
+                    <fieldset class='imgButtonStyle flex'>
+                        <legend class='absolute overflow-hidden h-1 w-1 m-[-1px] '></legend>
+                        {imgList.map((image, index) => (
+                            <label className='hover:cursor-pointer w-[160px] h-[90px]'>
+                                <input type="radio" class='hidden' name='subimg' id='subimg' onChange={handleInputImg}
+                                       value={index}/>
+                                <img class='max-w-none w-[160px] h-[90px]' src={imgPath + "/" + image} onClick={OnClickimg}/>
+                            </label>
+                        ))}
+                    </fieldset>
                 </div>
             </div>
-      </div>
+        </div>
     )
 }
