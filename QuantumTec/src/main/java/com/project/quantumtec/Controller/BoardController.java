f64518853157309c7ba4a7a8fee6228f48a450fa
YeChanKang <yechan.dev@gmail.com>
2023-06-11 23:27:39 +0900

    feat: [#50] throws Exception 삭제

--- QuantumTec/src/main/java/com/project/quantumtec/Controller/BoardController.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Controller/BoardController.java
@@ -23,91 +23,91 @@ public class BoardController {
 
     // 게시물 리스트 조회
     @PostMapping("/list")
-    public List<ListResponseDTO> getPostSearchList(@RequestBody ListDTO request) throws Exception {
+    public List<ListResponseDTO> getPostSearchList(@RequestBody ListDTO request){
         return boardService.getPostSearchList(request);
     }
 
     // 게시물 조회
     @PostMapping("/view")
-    public ViewResponseDTO getPost(@RequestBody ViewDTO request) throws Exception {
+    public ViewResponseDTO getPost(@RequestBody ViewDTO request){
         return boardService.getPost(request);
     }
 
     // 게시물 작성
     @PostMapping("/write")
-    public boolean writePost(@RequestBody WriteDTO request) throws Exception {
+    public boolean writePost(@RequestBody WriteDTO request){
         return boardService.writePost(request);
     }
 
     // 게시물 수정
     @PostMapping("/modify")
-    public boolean modifyPost(@RequestBody ModifyDTO request) throws Exception {
+    public boolean modifyPost(@RequestBody ModifyDTO request){
         return boardService.modifyPost(request);
     }
 
     // 게시물 삭제
     @PostMapping("/delete")
-    public boolean deletePost(@RequestBody DeleteDTO request) throws Exception {
+    public boolean deletePost(@RequestBody DeleteDTO request){
         return boardService.deletePost(request);
     }
 
     // 게시물 추천
     @PostMapping("/upvote")
-    public boolean upvotePost(@RequestBody VoteDTO request) throws Exception {
+    public boolean upvotePost(@RequestBody VoteDTO request){
         return boardService.upvotePost(request);
     }
 
     // 게시물 비추천
     @PostMapping("/downvote")
-    public boolean downvotePost(@RequestBody VoteDTO request) throws Exception {
+    public boolean downvotePost(@RequestBody VoteDTO request){
         return boardService.downvotePost(request);
     }
 
     // 다음 게시글 불러오기
     @PostMapping("/next")
-    public int getNextPost(@RequestBody NavigateViewDTO request) throws Exception {
+    public int getNextPost(@RequestBody NavigateViewDTO request){
         return boardService.getNextPost(request);
     }
 
     // 이전 게시글 불러오기
     @PostMapping("/prev")
-    public int getPrevPost(@RequestBody NavigateViewDTO request) throws Exception {
+    public int getPrevPost(@RequestBody NavigateViewDTO request){
         return boardService.getPrevPost(request);
     }
 
     // 댓글 불러오기
     @PostMapping("/commentList")
-    public List<CommentListResponseDTO> getCommentList(@RequestBody com.project.quantumtec.DTO.Request.board.CommentListDTO request) throws Exception {
+    public List<CommentListResponseDTO> getCommentList(@RequestBody com.project.quantumtec.DTO.Request.board.CommentListDTO request){
         return boardService.getCommentList(request);
     }
 
     // 댓글 작성
     @PostMapping("/commentWrite")
-    public boolean writeComment(@RequestBody CommentWriteDTO request) throws Exception {
+    public boolean writeComment(@RequestBody CommentWriteDTO request){
         return boardService.writeComment(request);
     }
 
     // 댓글 수정
     @PostMapping("/commentModify")
-    public boolean modifyComment(@RequestBody CommentModifyDTO request) throws Exception {
+    public boolean modifyComment(@RequestBody CommentModifyDTO request){
         return boardService.modifyComment(request);
     }
 
     // 댓글 삭제
     @PostMapping("/commentDelete")
-    public boolean deleteComment(@RequestBody CommentDeleteDTO request) throws Exception {
+    public boolean deleteComment(@RequestBody CommentDeleteDTO request){
         return boardService.deleteComment(request);
     }
 
     // 댓글 추천
     @PostMapping("/commentUpvote")
-    public boolean upvoteComment(@RequestBody CommentVoteDTO request) throws Exception {
+    public boolean upvoteComment(@RequestBody CommentVoteDTO request){
         return boardService.upvoteComment(request);
     }
 
     // 댓글 비추천
     @PostMapping("/commentDownvote")
-    public boolean downvoteComment(@RequestBody CommentVoteDTO request) throws Exception {
+    public boolean downvoteComment(@RequestBody CommentVoteDTO request){
         return boardService.downvoteComment(request);
     }
 
