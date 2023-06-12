bef9952b3a98fea9d480628eba5f78482a73cebc
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-06-13 03:54:10 +0900

    feat:[#13] 이미지가 추가되지 않은 게임들은 test 속 더미 이미지를 출력

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/GamePage/gamepage.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/GamePage/gamepage.js
@@ -310,7 +310,7 @@ export default function GamePage() {
                                                value={index}
                                                onClick={()=>gameLink(image.gameIndex,image.gameName) }/>
                                         <img class='max-w-none w-[320px] h-[180px]'
-                                             src={ `http://localhost:9090/image/game/games_${image.gameIndex}_0.png` }
+                                             src={ `http://localhost:9090/image/game/games_${image.gameIndex = image.gameIndex > 4 ? 'test' : image.gameIndex}_0.png` }
                                              value={ `http://localhost:9090/game${image.gameIndex}/${image.gameName}`}/>
                                         <div class='mt-2 font-bold'>{image.gameName}</div>
                                         <div>{image.gamePrice}</div>
