46fae02f445c6a1df8b3b425777a778f55866a48
MayoneJY <32291392+MayoneJY@users.noreply.github.com>
2023-08-06 18:40:58 +0900

    fix: [#50] 글쓰기 에디터(toast UI Editor)로 변경

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/BoardPage/write.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/BoardPage/write.js
@@ -1,35 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Editor from '@toast-ui/editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
 
 export default function WritePage() {
     const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
 
     const handleTitleChange = (event) => {
         setTitle(event.target.value);
     };
 
-    const handleContentChange = (event) => {
-        setContent(event.target.value);
-    };
-
     const handleSubmit = (event) => {
         event.preventDefault();
-        // Send the title and content to the server to be saved
-        // ...
+        //editor.getMarkdown()
     };
+
+    let editor;
+
+    useEffect(() => {
+        editor = new Editor({
+            el: document.querySelector('#editor'),
+            height: '600px',
+            initialEditType: 'markdown',
+            previewStyle: 'vertical'
+        });
+    }, []);
+
 
     return (
         <div className="container">
-            <h1 class="m-5">글쓰기</h1>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className=' mt-[10%]'>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="floatingInput" placeholder="title" onChange={handleTitleChange} />
                     <label for="floatingInput">제목</label>
                 </div>
-                <div class="form-floating">
-                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style={{height: "400px"}} onChange={handleContentChange} />
-                    <label for="floatingTextarea">내용</label>
-                </div>
+                <div id="editor" className='text-left'></div>
                 <br />
                 <button type="submit" class="btn btn-success mb-5">Submit</button>
             </form>
