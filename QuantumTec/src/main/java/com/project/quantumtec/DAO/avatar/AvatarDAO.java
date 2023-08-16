511e150e4d14105839fd6904fc9e408827bbd005
MayoneJY <mayone6063@kakao.com>
2023-08-16 23:52:03 +0900

    fix: [#83] 폴더 위치 변경

--- QuantumTec/src/main/java/com/project/quantumtec/DAO/avatar/AvatarDAO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DAO/avatar/AvatarDAO.java
@@ -2,7 +2,7 @@ package com.project.quantumtec.DAO.avatar;
 
 import java.util.List;
 
-import com.project.quantumtec.DTO.Request.avatar.AvatarInventoryDTO;
+import com.project.quantumtec.DTO.Response.avatar.AvatarInventoryDTO;
 
 /**
  * PackageName : com.project.quantumtec.DAO.avatar
