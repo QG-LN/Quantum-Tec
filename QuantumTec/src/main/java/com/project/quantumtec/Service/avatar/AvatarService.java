5859c2b2e0b346c57234cff31474ba9446862cc7
MayoneJY <mayone6063@kakao.com>
2023-08-17 02:58:10 +0900

    feat: [#83] 인벤토리 검색 기능 추가.

--- QuantumTec/src/main/java/com/project/quantumtec/Service/avatar/AvatarService.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Service/avatar/AvatarService.java
@@ -3,6 +3,7 @@ package com.project.quantumtec.Service.avatar;
 import java.util.List;
 
 import com.project.quantumtec.DTO.Request.avatar.CategoryInventoryDTO;
+import com.project.quantumtec.DTO.Request.avatar.InventorySearchDTO;
 import com.project.quantumtec.DTO.Response.avatar.AvatarInventoryDTO;
 
 public interface AvatarService {
@@ -15,4 +16,7 @@ public interface AvatarService {
 
     // 아바타 카테고리 별 인벤토리 정보 조회
     public List<AvatarInventoryDTO> getAvatarCategoryInventory(CategoryInventoryDTO categoryInventoryDTO);
+
+    // 아바타 searchValue로 인벤토리 정보 조회
+    public List<AvatarInventoryDTO> getAvatarSearchInventory(InventorySearchDTO inventorySearchDTO);
 }
