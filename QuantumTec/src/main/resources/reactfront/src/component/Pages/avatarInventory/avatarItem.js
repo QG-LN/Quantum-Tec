f785065bbb4bb07662bea917e9e502b3d42485a0
MayoneJY <mayone6063@kakao.com>
2023-08-20 03:55:12 +0900

    feat: [#83] 아이템 툴팁 기능 추가.

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/avatarInventory/avatarItem.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/avatarInventory/avatarItem.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import React, { useState, useEffect, useRef } from 'react';
+import Tooltip from '../../tooltip';
+import { OverlayTrigger } from 'react-bootstrap';
 
 /**
  * 아바타 아이템을 보여주는 컴포넌트
@@ -15,7 +15,7 @@ import Modal from 'react-bootstrap/Modal';
  * @author MayoneJY <mayone6063@kakao.com>
  */
 export default function AvatarItem(props) {
-    
+
     // 모달을 보여주기 위한 상태
     const [show, setShow] = useState(false);
     // 모달을 닫기 위한 함수
@@ -32,81 +32,37 @@ export default function AvatarItem(props) {
         console.log(e);
     }
 
+    const tooltip = (
+        <div className='text-left m-3'>
+            <h4>파란색 배경</h4><br />
+            구매일 : 2023-08-16 22:54:50<br />
+            분류 : 배경<br />
+            파란색 배경 아이템입니다.<br />
+        </div>
+    );
+
     return (
         <div className="card w-[18.5%] ms-2 placeholder-glow mb-4" aria-hidden="true">
-            <Modal show={show} onHide={handleClose} centered={true}>
-                <Modal.Header>
-                    <Modal.Title className='w-[100%]'>
-                        <div className='row justify-content-between'>
-                            <div className='col-2'>
-                                구매
-                            </div>
-                            <div className='col-6 row justify-content-between'>
-                                <h3 className="card-text placeholder-glow col-6">
-                                    <div className="placeholder col-12"></div>
-                                </h3>
-                                <h3 className="card-text placeholder-glow col-6">
-                                    <div className="placeholder col-12"></div>
-                                </h3>
-                            </div>
-                        </div>
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <div className='row justify-content-between'>
-                        <div className='placeholder-glow col-5'>
-                            {/* <div class="placeholder ratio ratio-1x1 rounded"></div> */}
-                            <img src={imgSrc} className='rounded'/>
-                        </div>
-                        <div className='col-7'>
+                
+            <Tooltip content={tooltip}>
+                <div onClick={handleShow}>
+                    {/* <div className="placeholder ratio ratio-1x1 rounded-top"></div> */}
+                    <img src={imgSrc} className='rounded-top'/>
+                    {/* <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" class="card-img-top" alt="..."/> */}
+                    <div className="text-start m-3">
+                        <h5 className="card-title placeholder-glow">
                             {props.item && props.item.itemName ? props.item.itemName : "No item name available"}
-                            <h6 className="card-text placeholder-glow">
-                                <div className="placeholder col-5"></div> 제작
-                            </h6>
-                            <h6 className="card-text placeholder-glow">
-                                <div className="placeholder col-8"></div>
-                            </h6>
-                            <h6 className="card-text placeholder-glow">
-                                <div className="placeholder col-2"></div>
-                                <div className="placeholder ms-2 col-5"></div>
-                            </h6>
-                            <br/>
-                            <br/>
-                            <h6 className="card-text placeholder-glow">
-                                <div className="placeholder col-4"></div>
-                            </h6>
-                        </div>
+                        {/* <div className="placeholder col-5"></div> */}
+                        </h5>
+                        <h6 className="card-text placeholder-glow">
+                            <div className="placeholder col-7"></div>
+                        </h6>
+                        <h6 className="card-text placeholder-glow text-end">
+                            <div className="placeholder col-5"></div>
+                        </h6>
                     </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button className="btn_close" variant="success" onClick={handleClose}>
-                        10,000 freecash
-                    </Button>
-                    <Button className="btn_close" variant="success" onClick={handleClose}>
-                        1,000 cash
-                    </Button>
-                    <Button className="btn_close" variant="secondary" onClick={handleClose}>
-                        닫기
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-            <div onClick={handleShow}>
-                {/* <div className="placeholder ratio ratio-1x1 rounded-top"></div> */}
-                <img src={imgSrc} className='rounded-top'/>
-                {/* <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" class="card-img-top" alt="..."/> */}
-                <div className="text-start m-3">
-                    <h5 className="card-title placeholder-glow">
-                        {props.item && props.item.itemName ? props.item.itemName : "No item name available"}
-                    {/* <div className="placeholder col-5"></div> */}
-                    </h5>
-                    <h6 className="card-text placeholder-glow">
-                        <div className="placeholder col-7"></div>
-                    </h6>
-                    <h6 className="card-text placeholder-glow text-end">
-                        <div className="placeholder col-5"></div>
-                    </h6>
                 </div>
-            </div>
+            </Tooltip>
         </div>
     );
 }
---
+++ QuantumTec/src/main/resources/reactfront/src/component/tooltip.js
@@ -0,0 +1,62 @@
+import React, { useState, useRef, useEffect } from 'react';
+import './tooltip.css';
+
+/**
+ * 툴팁 컴포넌트
+ * @param {JSX.Element} props.content - 툴팁에 들어갈 내용
+ * @param {JSX.Element} props.children - 툴팁을 보여줄 컴포넌트
+ * @returns {JSX.Element} - Tooltip 컴포넌트
+ * @auther MayoneJY <mayone6063@kakao.com>
+ */
+export default function Tooltip({ content, children }) {
+    const [showTooltip, setShowTooltip] = useState(false);
+    const tooltipContainerRef = useRef(null);
+    const tooltipRef = useRef(null);
+    const [tooltipPosition, setTooltipPosition] = useState('right');
+    const [parentSize, setParentSize] = useState({ width: 0, height: 0 });
+
+    const handleMouseEnter = () => {
+        if (tooltipContainerRef.current) {
+            const { width, height } = tooltipContainerRef.current.getBoundingClientRect();
+            setParentSize({ width, height });
+        }
+        setShowTooltip(true);
+    };
+
+    const handleMouseLeave = () => {
+        setShowTooltip(false);
+    };
+
+    useEffect(() => {
+        if (tooltipContainerRef.current && tooltipRef.current && showTooltip) {
+            const containerRect = tooltipContainerRef.current.getBoundingClientRect();
+            const tooltipRect = tooltipRef.current.getBoundingClientRect();
+
+            if (window.innerWidth - containerRect.right < tooltipRect.width) {
+                setTooltipPosition('left');
+            } else {
+                setTooltipPosition('right');
+            }
+            console.log(window.innerWidth, containerRect.right, tooltipRect.width )
+        }
+    }, [showTooltip]);
+
+    return (
+        <div 
+        className="tooltip-container" 
+        onMouseEnter={handleMouseEnter} 
+        onMouseLeave={handleMouseLeave}
+        ref={tooltipContainerRef}
+        >
+        {children}
+        {showTooltip && (
+            <div className={`tooltip-box ${tooltipPosition}`}
+                style={{ width: parentSize.width+parentSize.width+10, height: parentSize.height }}
+                ref={tooltipRef}
+            >
+                {content}
+            </div>
+        )}
+        </div>
+    );
+}
