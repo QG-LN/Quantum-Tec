0ab6aa275a0f5b778ca695092f983f104b169a43
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-09-09 20:57:20 +0900

    feat[#50]: 글쓰기 버튼 클릭 시 글 등록

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/BoardPage/write.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/BoardPage/write.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import {axiosRequest} from '../../../module/networkUtils';
+
 
 export default function WritePage() {
     const [title, setTitle] = useState('');
@@ -14,8 +16,38 @@ export default function WritePage() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        // Send the title and content to the server to be saved
-        // ...
+        
+        if(!title){
+            alert('제목을 입력하세요.');
+            return;
+        }else if(!content){
+            alert('내용을 입력하세요.');
+            return;
+        }else{
+            const currentPathname = window.location.pathname; // 현재 주소값 가져오기
+            const parts = currentPathname.split('/'); // 슬래시(/)를 기준으로 주소를 자르기
+            const boardId = parts[2]; // boardId 값을 추출
+
+            const userID = localStorage.getItem("userID");
+
+            const path = 'http://localhost:9090/board/write';
+            const body = {
+                boardIndex : boardId,
+                userID : userID,
+                title : title,
+                content : content,
+            }
+            const result = axiosRequest(path, body, 'POST', 'json');
+            if(result){
+                alert('글이 등록되었습니다.');
+                document.location.href = `/board/${boardId}`;
+            }else{
+                alert('글 등록에 실패하였습니다.');
+            }
+            
+        }
+
+
     };
 
     return (
