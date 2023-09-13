4e97c81bba9c1db76cf7f1601c5ec8fa386ec7e9
MayoneJY <mayone6063@kakao.com>
2023-09-13 16:13:33 +0900

    fix: [#82] 결제 완료가 되고 내 정보 리로드가 안되던 문제.

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/PaymentsPage/success.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/PaymentsPage/success.js
@@ -1,10 +1,16 @@
 import { useSearchParams } from "react-router-dom"
 import React, { useEffect } from 'react';
 import { axiosRequest } from "../../../module/networkUtils";
+import { useDispatch } from 'react-redux';
+import { setCashChange } from '../../../redux/actions/userActions';
 
 export function Success() {
     const [searchParams] = useSearchParams()
 
+    const dispatch = useDispatch();
+    const handleUpdate = (newCash) => {
+        dispatch(setCashChange(newCash));
+    };
     // 서버로 승인 요청
     useEffect(() => {
         // 결제 승인 요청
@@ -25,7 +31,9 @@ export function Success() {
                     else if(res === -1){
                         alert('결제 승인에 실패했습니다.');
                     }
-                    localStorage.setItem("userCash", res.userCash);
+                    handleUpdate(res);
+                    localStorage.setItem("userCash", res);
+
                 }
                 else{
                     alert('충전에 실패했습니다.');
