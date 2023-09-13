d00822adce7223d7c88b667a7fcaad811e230453
MayoneJY <mayone6063@kakao.com>
2023-09-13 17:26:48 +0900

    fix: [#107] 아바타 상점 모두 보기 개수가 변동되지 않는 현상 해결.

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/avatarInventory/avatarMainShop.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/avatarInventory/avatarMainShop.js
@@ -5,32 +5,38 @@ import { Carousel } from 'react-bootstrap';
 
 export default function AvatarMainInventory(props) {
     const avatarCategoryList = useSelector(state => state.avatar.categoryList);
-    
+
     if (!avatarCategoryList) {
         return null;
     }
-
     const ShowCarousel = (c) => {
         const firstList = props.itemList.filter(item => item.itemCategoryName === c.category).slice(0, 5); // 0부터 4까지
         const secondList = props.itemList.filter(item => item.itemCategoryName === c.category).slice(5, 10); // 5부터 9까지
+        const listSize = firstList.length + secondList.length;
         // const firstList = props.itemList.slice(0, 5); // 0부터 4까지
         // const secondList = props.itemList.slice(5, 10); // 5부터 9까지
-        console.log(secondList.length)
         return (
-            <Carousel interval={null}>
-                <Carousel.Item>
-                    <div className='ms-[3%] my-4 d-flex flex-wrap align-items-center'>
-                        <AvatarItemCheck itemList={firstList} refreshKey={props.refreshKey}/>
-                    </div>
-                </Carousel.Item>
-                {secondList.length === 0 ? null :
-                <Carousel.Item>
-                    <div className='ms-[3%] my-4 d-flex flex-wrap align-items-center'>
-                        <AvatarItemCheck itemList={secondList} refreshKey={props.refreshKey}/>
-                    </div>
-                </Carousel.Item>
-                }
-            </Carousel>
+            <>
+                <div className='d-flex align-items-center'>
+                    <h5 className={c.category.length > 2 ? 'w-[20%]' : 'w-[18%]'}>모든 {c.category} 아이템</h5>
+                    <button type="button" class="btn btn-secondary btn-sm" id={c.category} onClick={props.onClick}>모두 보기 ({listSize})</button>
+                    <hr className='flex-fill mx-3'/>
+                </div>
+                <Carousel interval={null}>
+                    <Carousel.Item>
+                        <div className='ms-[3%] my-4 d-flex flex-wrap align-items-center'>
+                            <AvatarItemCheck itemList={firstList} refreshKey={props.refreshKey}/>
+                        </div>
+                    </Carousel.Item>
+                    {secondList.length === 0 ? null :
+                    <Carousel.Item>
+                        <div className='ms-[3%] my-4 d-flex flex-wrap align-items-center'>
+                            <AvatarItemCheck itemList={secondList} refreshKey={props.refreshKey}/>
+                        </div>
+                    </Carousel.Item>
+                    }
+                </Carousel>
+            </>
         );
     }
     console.log(avatarCategoryList)
@@ -38,11 +44,6 @@ export default function AvatarMainInventory(props) {
         <>
             {avatarCategoryList.map((category) => (
                 <div className='mt-2 mb-1 me-3'>
-                    <div className='d-flex align-items-center'>
-                        <h5 className={category.length > 2 ? 'w-[20%]' : 'w-[18%]'}>모든 {category} 아이템</h5>
-                        <button type="button" class="btn btn-secondary btn-sm" id={category} onClick={props.onClick}>모두 보기 ({avatarCategoryList.length})</button>
-                        <hr className='flex-fill mx-3'/>
-                    </div>
                     <ShowCarousel category={category}/>
                 </div>
             ))}
