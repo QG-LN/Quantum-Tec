454ac976a0b288cfaf0585772d8732e3bedd6abe
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-09-17 00:42:28 +0900

    fix[#119]: 게시판페이지에서 게시판 변경 시 아바타 초기화 문제 해결

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/BoardPage/board.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/BoardPage/board.js
@@ -246,10 +246,18 @@ export default function Board() {
                             {boardName}
                         </button>
                         <ul class="dropdown-menu w-[100%]" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item" href="/board/0">전체</a></li>
-                            <li><a class="dropdown-item" href="/board/1">자유</a></li>
-                            <li><a class="dropdown-item" href="/board/2">튜터링</a></li>
-                            <li><a class="dropdown-item" href="/board/3">공지사항</a></li>
+                            <li>
+                                <Link to={`/board/0`} className="dropdown-item hover:cursor-pointer">전체</Link>
+                            </li>
+                            <li>
+                                <Link to={`/board/1`} className="dropdown-item hover:cursor-pointer">자유</Link>
+                            </li>
+                            <li>
+                                <Link to={`/board/2`} className="dropdown-item hover:cursor-pointer">튜터링</Link>
+                            </li>
+                            <li>
+                                <Link to={`/board/3`} className="dropdown-item hover:cursor-pointer">공지사항</Link>
+                            </li>
                         </ul>
                     </div>
                     <div class="dropdown col-auto">
