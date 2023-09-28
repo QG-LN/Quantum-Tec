6041732ab8bc75f3fc652396227eee1f24617c36
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-09-28 20:32:14 +0900

    design[#9]: 기본적인 결제 내역 디자인 완료

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/MyPage/MyPaymentDetails.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/MyPage/MyPaymentDetails.js
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
 import {axiosRequest} from '../../../module/networkUtils';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleQuestion} from "@fortawesome/free-solid-svg-icons";
 
+
+/**
+ * @todo 결제 내역 페이징 처리 및 검색 기능 + 정렬 기능 + 아이콘 튤팁 추가
+ */
 export default function MyPaymentDetails() {
   const [data, setData] = useState([]);
   const [isState, setState] = useState("isMonthly");
@@ -26,6 +32,8 @@ export default function MyPaymentDetails() {
     };
     const data = await axiosRequest(path,body,'POST','json');
     setpaymentPosts(data);
+
+    console.log(data);
   };
 
   //월일을 클릭 시
@@ -68,23 +76,41 @@ export default function MyPaymentDetails() {
     );
   };
 
-    /**
-     * 날짜 데이터 포맷 변경 함수
-     * 만약 데이터가 날짜 형식이 아니라면 그대로 반환
-     * @param data 날짜데이터
-     * @returns {*|string} 포맷 변경된 날짜 데이터
-     * @example 2021-06-01 -> 2021년 06월 01일
-     */
-    const extractData = (data) => {
-      const datePattern = /^(\d{4})-(\d{2})-(\d{2})/;     // 날짜 데이터 추출 패턴 설정
-      const match = datePattern.exec(data);       // 정규식과 매칭을 통해 날짜 데이터 추출
+  /**
+  * 날짜 데이터 포맷 변경 함수
+  * 만약 데이터가 날짜 형식이 아니라면 그대로 반환
+  * @param data 날짜데이터
+  * @returns {*|string} 포맷 변경된 날짜 데이터
+  * @example 2021-06-01 -> 2021년 06월 01일
+  */
+  const extractData = (data) => {
+    const datePattern = /^(\d{4})-(\d{2})-(\d{2})/;     // 날짜 데이터 추출 패턴 설정
+    const match = datePattern.exec(data);       // 정규식과 매칭을 통해 날짜 데이터 추출
 
-      // 날짜 데이터를 올바르게 추출 했을 경우 포맷 변경
-      if(match){
-          return match[1] + '년 ' + match[2] + '월 ' + match[3] + '일';
-      }else{
-          return data;
-      }
+    // 날짜 데이터를 올바르게 추출 했을 경우 포맷 변경
+    if(match){
+        return match[1] + '년 ' + match[2] + '월 ' + match[3] + '일';
+    }else{
+        return data;
+    }
+  }
+
+  /**
+   * 결제 상태에 따라서 텍스트 색상을 변경하는 함수
+   * @param {string} status 결제 상태를 나타내는 문자열 
+   * @returns <span>태그를 반환하는 함수
+   */
+  const changePaymentStatusText = (status) => {
+    switch(status){
+      case "결제 완료":
+        return <span class='text-primary'>{status}</span>; 
+      case "결제 취소":
+        return <span class='text-danger'>{status}</span>;
+      case "결제 환불":
+        return <span class='text-warning'>{status}</span>;
+      default:
+        return <span class='text-primary'>{status}</span>
+    }
   }
 
   /**
@@ -93,81 +119,85 @@ export default function MyPaymentDetails() {
   const renderPaymentPosts = () => {
     return paymentPosts.map((paymentHistory, index) => (
       <tr key={index} style={{cursor:'pointer'}}>
-              <td>{paymentHistory.paymentIndex}</td>
-              <td>{paymentHistory.paymentDesc}</td>
-              <td>{extractData(paymentHistory.paymentDate)}</td>
-              <td>{paymentHistory.paymentType}</td>
-              <td class='text-primary'>{paymentHistory.paymentStatus}</td>
+        <td>{paymentHistory.paymentIndex}</td>
+        <td>{paymentHistory.paymentDesc} [{paymentHistory.paymentAmount}]</td>
+        <td>{extractData(paymentHistory.paymentDate)}</td>
+        <td>{paymentHistory.paymentType}</td>
+        <td>{changePaymentStatusText(paymentHistory.paymentStatus)}</td>
       </tr>
     ));
   };
 
+  const renderPaymentSearch = () => {
+    return (
+      <div className="row justify-content-center">
+        <div class="dropdown col-auto">
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="pdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            검색
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li><span class="dropdown-item hover:cursor-pointer">제목</span></li>
+            <li><span class="dropdown-item hover:cursor-pointer">작성자</span></li>
+            <li><span class="dropdown-item hover:cursor-pointer">제목 + 작성자</span></li>
+          </ul>
+        </div>
+        <div class="col-auto">
+          <label for="inputSearch" class="visually-hidden">검색어</label>
+          <input type="text" class="form-control" id="inputSearch" placeholder="검색어"></input>
+        </div>
+        <div class="col-auto">
+          <button type="submit" class="btn btn-success mb-3">검색</button>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div class="mypagestyle float-right w-mypagesection max-w-[880px] relative min-w-[700px]">
       <h2 class="account_main_page_title ">결제 내역</h2>
-      <div>
-        <div class='h-[50px] border bg-gray-200 m-3'>
-          정렬 / 검색 영역
-        </div>
-      </div>
-      <table className="table table-striped mt-0 pt-0 table-hover user-select-none">
+      <table className="table table-striped mt-0 pt-0 table-hover user-select-none border-top-2">
           <thead>
             <tr>
               <th className="w-[10%]">번호</th>
-              <th className="w-[50%]">결제내역</th>
+              <th className="w-[45%]">결제내역</th>
               <th className="w-[20%]">결제일자</th>
-              <th className="w-[10%]">결제유형</th>
-              <th className="w-[15%]">결제상태</th>
+              <th className="w-[15%]">
+                결제유형<FontAwesomeIcon icon={faCircleQuestion} style={{color: "#8f8f8f"}}/>
+              </th>
+              <th className="w-[10%]">결제상태</th>
             </tr>
           </thead>
           <tbody>{renderPaymentPosts()}</tbody>
         </table>
         <nav aria-label="Page navigation example mb-5">
-                <div className="row justify-content-center">
-                    <div class="dropdown col-auto">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                            검색
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><span class="dropdown-item hover:cursor-pointer">제목</span></li>
-                            <li><span class="dropdown-item hover:cursor-pointer">작성자</span></li>
-                            <li><span class="dropdown-item hover:cursor-pointer">제목 + 작성자</span></li>
-                        </ul>
-                    </div>
-                    <div class="col-auto">
-                        <label for="inputSearch" class="visually-hidden">검색어</label>
-                        <input type="text" class="form-control" id="inputSearch" placeholder="검색어"></input>
-                    </div>
-                    <div class="col-auto">
-                        <button type="submit" class="btn btn-success mb-3">검색</button>
-                    </div>
-                </div>
-                <ul className="pagination nav justify-content-center mb-10">
-                    {startPageNum !== 1 && 
-                        <li key="<" className="page-item nav-item">
-                            <button className="page-link nav-item">
-                                {"<"}
-                            </button>
-                        </li>
-                    }
-                    {/* 페이징 처리를 위한 컴포넌트 추가 */}
-                    {Array.from({ length: Math.min(10, Math.ceil(postCount/ ITEMS_PER_PAGE) - startPageNum + 1) }, (_, i) => startPageNum + i).map(pageNumber => (
-                        <li key={pageNumber} className={`page-item nav-item ${pageNumber === currentPage ? 'active' : ''}`}>
-                            <button className="page-link nav-item">
-                                {pageNumber}
-                            </button>
-                        </li>
-                    ))}
-
-                    {Math.ceil(postCount / ITEMS_PER_PAGE) > Math.floor((currentPage-1) / ITEMS_PER_PAGE + 1) * 10 &&
-                        <li key=">" className="page-item nav-item">
-                            <button className="page-link nav-item">
-                                {">"}
-                            </button>
-                        </li>
-                    }
-                </ul>
-            </nav>
+          <div class="mt-4">
+            {renderPaymentSearch()}
+          </div>     
+          <ul className="pagination nav justify-content-center mb-10">
+            {startPageNum !== 1 && 
+              <li key="<" className="page-item nav-item">
+                <button className="page-link nav-item">
+                  {"<"}
+                </button>
+              </li>
+            }
+            {/* 페이징 처리를 위한 컴포넌트 추가 */}
+            {Array.from({ length: Math.min(10, Math.ceil(postCount/ ITEMS_PER_PAGE) - startPageNum + 1) }, (_, i) => startPageNum + i).map(pageNumber => (
+              <li key={pageNumber} className={`page-item nav-item ${pageNumber === currentPage ? 'active' : ''}`}>
+                <button className="page-link nav-item">
+                  {pageNumber}
+                </button>
+              </li>
+            ))}
+            {Math.ceil(postCount / ITEMS_PER_PAGE) > Math.floor((currentPage-1) / ITEMS_PER_PAGE + 1) * 10 &&
+              <li key=">" className="page-item nav-item">
+                <button className="page-link nav-item">
+                  {">"}
+                </button>
+              </li>
+            }
+          </ul>
+        </nav>
     </div>
   );
 }
