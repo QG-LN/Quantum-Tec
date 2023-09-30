2c4bdc9627df69bdb931773441204f5460662bbf
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-09-30 13:30:49 +0900

    refactor[#50]: 날짜 포맷 함수 분할에 따른 코드 수정

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/BoardPage/board.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/BoardPage/board.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link , useNavigate} from 'react-router-dom';
 import {axiosRequest} from '../../../module/networkUtils';
 import {useParams} from "react-router-dom";
+import { extractData } from "../../Utils/dataFormat";
 
 const ITEMS_PER_PAGE = 10; // 페이지 당 아이템 수
 export default function Board() {
@@ -79,25 +80,6 @@ export default function Board() {
                 return '전체';
         }
     };
-
-    /**
-     * 날짜 데이터 포맷 변경 함수
-     * 만약 데이터가 날짜 형식이 아니라면 그대로 반환
-     * @param data 날짜데이터
-     * @returns {*|string} 포맷 변경된 날짜 데이터
-     * @example 2021-06-01 -> 2021년 06월 01일
-     */
-    const extractData = (data) => {
-        const datePattern = /^(\d{4})-(\d{2})-(\d{2})/;     // 날짜 데이터 추출 패턴 설정
-        const match = datePattern.exec(data);       // 정규식과 매칭을 통해 날짜 데이터 추출
-
-        // 날짜 데이터를 올바르게 추출 했을 경우 포맷 변경
-        if(match){
-            return match[1] + '년 ' + match[2] + '월 ' + match[3] + '일';
-        }else{
-            return data;
-        }
-    }
 
     useEffect(() => {
         loadBoardList();
