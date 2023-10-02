a1074164837774442a3975dd53a29bc7801cda33
YeChanKang <yechan.dev@gmail.com>
2023-10-02 22:03:04 +0900

    feat: [#110] 튜터링 목록을 불러오기 위한 request, response DTO 생성

---
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/Response/board/TutoringListResponseDTO.java
@@ -0,0 +1,13 @@
+package com.project.quantumtec.DTO.Response.board;
+
+public class TutoringListResponseDTO {
+    private int postIndex;      // 게시글 번호
+    private String[] tags;        // 게시글 태그
+    private String postTitle;   // 게시글 제목
+    private String postIcon;    // 게시글 아이콘
+    private String postAuthor;  // 작성자 닉네임
+    private String avatar;      // 작성자 아바타
+    private String postDate;    // 게시글 등록일
+    private int participant;    // 튜터링 참여 인원
+    private String subject;     // 튜터링 과목
+}
