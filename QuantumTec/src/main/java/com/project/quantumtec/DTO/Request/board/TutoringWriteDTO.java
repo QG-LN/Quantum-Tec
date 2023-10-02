8ccc6054566528ee37ab9a44dd8c01d404a5fb02
YeChanKang <yechan.dev@gmail.com>
2023-10-02 22:22:29 +0900

    feat: [#110] TutoringWriteDTO의 수업 진행 방식을 저장하는 변수 타입을 bool에서 String으로 수정

--- QuantumTec/src/main/java/com/project/quantumtec/DTO/Request/board/TutoringWriteDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/Request/board/TutoringWriteDTO.java
@@ -7,7 +7,7 @@ public class TutoringWriteDTO {
     private String userID; // 유저 아이디
     private int postTitle; // 튜터링 게시물 제목
     private String type; // 모집 구분 (스터디/학습위주)
-    private boolean online; // 온라인 여부
+    private String online; // 온라인 여부
     private String contact; // 연락처 - 카톡 오픈채팅방 링크
     private String startDate; // 시작 날짜
     private String period; // 예상 기간
