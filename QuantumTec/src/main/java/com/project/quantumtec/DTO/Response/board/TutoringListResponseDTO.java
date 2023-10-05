1d8dc373fc9f4855ba8a1acbf3725751f15f2516
YeChanKang <yechan.dev@gmail.com>
2023-10-05 10:27:07 +0900

    feat: [#110] TutoringListResponseDTO 내부에 게시물 아이콘 추가

--- QuantumTec/src/main/java/com/project/quantumtec/DTO/Response/board/TutoringListResponseDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/Response/board/TutoringListResponseDTO.java
@@ -7,6 +7,7 @@ public class TutoringListResponseDTO {
     private int postIndex;      // 게시글 번호
     private String[] tags;        // 게시글 태그
     private String postTitle;   // 게시글 제목
+    private String postIcon; // 튜터링 게시물 아이콘
     private String userNickname;  // 작성자 닉네임
     private String userIcon;      // 작성자 아이콘
     private String postDate;    // 게시글 등록일
