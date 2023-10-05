1199c4a10ee794ab2a20d0ddf67ef54c4cdabc00
YeChanKang <yechan.dev@gmail.com>
2023-10-05 10:28:06 +0900

    feat: [#110] TutoringWriteDTO 내부 모집 구분 변수를 문자열 변수로 변경, 모집 분야를 삭제

--- QuantumTec/src/main/java/com/project/quantumtec/DTO/Request/board/TutoringWriteDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/Request/board/TutoringWriteDTO.java
@@ -6,12 +6,11 @@ import lombok.Data;
 public class TutoringWriteDTO {
     private String userID; // 유저 아이디
     private int postTitle; // 튜터링 게시물 제목
-    private String type; // 모집 구분 (스터디/학습위주)
+    private String[] type; // 모집 구분 (스터디/학습위주)
     private String online; // 온라인 여부
     private String contact; // 연락처 - 카톡 오픈채팅방 링크
     private String startDate; // 시작 날짜
     private String period; // 예상 기간
-    private String part; // 모집 분야
     private String participant; // 모집 인원
     private String[] subject; // 과목
     private String postInfo; // 튜터링 소개글
