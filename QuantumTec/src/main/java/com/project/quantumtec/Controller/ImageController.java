9a0b111bebd08143f2bbd514367b56afd67c8643
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-10-06 12:16:09 +0900

    fix[#124]:이미지가 로드 되지 않는 문제 해결

--- QuantumTec/src/main/java/com/project/quantumtec/Controller/ImageController.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Controller/ImageController.java
@@ -42,12 +42,16 @@ public class ImageController {
      * @param imagePath 이미지 경로 (ex: game_1_1.png -> game/1/1.png)를 의미
      * */
     @GetMapping("/game/list/{path:.+}")
-    public String  getImageList(@PathVariable(value = "path") String imagePath){
+    public String getImageList(@PathVariable(value = "path") String imagePath){
         try {
             List<String> imagePaths = new ArrayList<>();
             imagePath = imagePath.replaceAll("_", "/");            // _을 /로 변경
             String path = "static/images/" + imagePath;
-            File dir = new File("src/main/resources/" + path);
+
+            // 디렉터리 경로를 문자열로 직접 구성이 아닌 스프링에서 제공하는 함수로 연결
+            Resource resource = new ClassPathResource(path);
+            File dir = resource.getFile();
+
             File[] fileList = dir.listFiles();
             if (fileList != null) {
                 for (File file : fileList) {
@@ -62,10 +66,13 @@ public class ImageController {
             String json = objectMapper.writeValueAsString(imagePaths);
 
             return json;
+
         } catch (Exception e) {
             System.out.println("이미지가 없습니다.");
             System.out.println(e.getMessage());
             return null;
         }
     }
+
+
 }
