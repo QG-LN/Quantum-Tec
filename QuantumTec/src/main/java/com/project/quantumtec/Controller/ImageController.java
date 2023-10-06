3fc2fa1176c231afa7f2dbf71f95a632d3fe6510
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-10-06 12:21:48 +0900

    refactor[#124]: ResponseEntity를 활용하여 항목을 반환하도록 변경

--- QuantumTec/src/main/java/com/project/quantumtec/Controller/ImageController.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Controller/ImageController.java
@@ -3,6 +3,7 @@ package com.project.quantumtec.Controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
@@ -42,7 +43,7 @@ public class ImageController {
      * @param imagePath 이미지 경로 (ex: game_1_1.png -> game/1/1.png)를 의미
      * */
     @GetMapping("/game/list/{path:.+}")
-    public String getImageList(@PathVariable(value = "path") String imagePath){
+    public ResponseEntity<String> getImageList(@PathVariable(value = "path") String imagePath){
         try {
             List<String> imagePaths = new ArrayList<>();
             imagePath = imagePath.replaceAll("_", "/");            // _을 /로 변경
@@ -50,9 +51,10 @@ public class ImageController {
 
             // 디렉터리 경로를 문자열로 직접 구성이 아닌 스프링에서 제공하는 함수로 연결
             Resource resource = new ClassPathResource(path);
-            File dir = resource.getFile();
+            File dir = resource.getFile();      // 경로에서 파일 객체를 가져옴
+            File[] fileList = dir.listFiles();  // 파일 객체에서 파일 리스트를 가져옴
 
-            File[] fileList = dir.listFiles();
+            // 파일 리스트에서 파일을 하나씩 꺼내서 파일이면 이미지 경로를 리스트에 추가
             if (fileList != null) {
                 for (File file : fileList) {
                     if (file.isFile()) {
@@ -65,12 +67,11 @@ public class ImageController {
             ObjectMapper objectMapper = new ObjectMapper();
             String json = objectMapper.writeValueAsString(imagePaths);
 
-            return json;
+            return ResponseEntity.ok().body(json);
 
         } catch (Exception e) {
-            System.out.println("이미지가 없습니다.");
             System.out.println(e.getMessage());
-            return null;
+            return ResponseEntity.notFound().build();
         }
     }
 
