77b1f474ac7908c21c6fdc5e55e32acd6cd352b2
MayoneJY <mayone6063@kakao.com>
2023-10-11 15:28:41 +0900

    fix: [#125] 반응형이 제대로 작동하지 않던 문제 수정.

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/test.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/test.js
@@ -22,13 +22,34 @@ import { Grid, Container, Typography } from '@mui/material';
 import Nav from '../../../dashboard/layouts/dashboard/nav';
 import Header from '../../../dashboard/layouts/dashboard/header';
 import { useState } from 'react';
+import { styled } from '@mui/material/styles';
+const APP_BAR_MOBILE = 64;
+const APP_BAR_DESKTOP = 92;
+const Main = styled('div')(({ theme }) => ({
+  flexGrow: 1,
+  overflow: 'auto',
+  minHeight: '100%',
+  paddingTop: APP_BAR_MOBILE + 24,
+  paddingBottom: theme.spacing(10),
+  [theme.breakpoints.up('lg')]: {
+    paddingTop: APP_BAR_DESKTOP + 24,
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+  },
+}));
+const StyledRoot = styled('div')({
+  display: 'flex',
+  minHeight: '100%',
+  overflow: 'hidden',
+});
 function Test() {
     const [open, setOpen] = useState(false);
     const theme = useTheme();
     return (
-        <div className='ms-[30vh]'>
+        <StyledRoot>
             <Header onOpenNav={() => setOpen(true)} />
             <Nav openNav={open} onCloseNav={() => setOpen(false)} />
+            <Main>
             <ThemeProvider>
                 <ScrollToTop />
                 <StyledChart />
@@ -228,7 +249,9 @@ function Test() {
                     </Grid>
                 </Container>
             </ThemeProvider>
-        </div>
+            </Main>
+            
+        </StyledRoot>
     );
 }
 
