0460830fbe19b0b26169f05b56ce277a859ab149
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-10-15 21:00:08 +0900

    add[#110]: 카테고리 전송을 위한 ResponseDTO 추가

---
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/Response/board/TutoringCategoryListResponseDTO.java
@@ -0,0 +1,12 @@
+package com.project.quantumtec.DTO.Response.board;
+
+import lombok.Data;
+
+
+/**
+ * @todo 추후 추가해야할 내용이 있지만 임시로 재워두기
+ */
+@Data
+public class TutoringCategoryListResponseDTO {
+    private String category;          // 튜터링 카테고리
+}
