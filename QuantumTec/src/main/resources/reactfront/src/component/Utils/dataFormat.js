3d46253f0de2985cdba9fb49fe7563da397c3c99
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-10-15 19:44:03 +0900

    modify: 날짜 데이터 포맷함수를 입력받은 값에 맞게 수정할 수 있도록 수정
    추후 좀 더 최적화 필요

--- QuantumTec/src/main/resources/reactfront/src/component/Utils/dataFormat.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Utils/dataFormat.js
@@ -5,13 +5,19 @@
  * @returns {*|string} 포맷 변경된 날짜 데이터
  * @example 2021-06-01 -> 2021년 06월 01일
  */
-export function extractData(data) {
+export function extractData(data, format) {
   const datePattern = /^(\d{4})-(\d{2})-(\d{2})/; // 날짜 데이터 추출 패턴 설정
   const match = datePattern.exec(data); // 정규식과 매칭을 통해 날짜 데이터 추출
 
+  console.log(format);
+
   // 날짜 데이터를 올바르게 추출 했을 경우 포맷 변경
   if (match) {
-    return match[1] + "년 " + match[2] + "월 " + match[3] + "일";
+    if(format === null && format === undefined){
+      return match[1] + "년 " + match[2] + "월 " + match[3] + "일";
+    }else{
+      return match[1] + format + match[2] + format + match[3];
+    }
   } else {
     return data;
   }
