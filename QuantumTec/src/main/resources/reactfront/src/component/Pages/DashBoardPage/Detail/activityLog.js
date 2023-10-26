097e603eb339dcf5fce7a5ca511319d683119e9a
MayoneJY <mayone6063@kakao.com>
2023-10-26 21:34:57 +0900

    fix: [#125] 활동 로그 테이블 헤더 변경.

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/activityLog.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/activityLog.js
@@ -1,27 +1,48 @@
 import React from 'react';
 
 function ActivityLog() {
-    const activityLog = {
-        lastLogin: "2022-10-01",
-        mainGameCategory: "RPG",
-        commentCount: 50
-    };
+    // 변경 사항, 변경 내용, 변경 자, 변경 시간
+    const activityLog = [{
+        changeDetails: "게시글",
+        changeDescription: "제목 -> 제목1",
+        changer: "홍길동",
+        changeTime: "2022-01-01 00:00:00"
+    },
+    {
+        changeDetails: "댓글",
+        changeDescription: "안녕하세요 -> 아니에요",
+        changer: "홍길동",
+        changeTime: "2022-01-01 00:00:00"
+    },
+    {
+        changeDetails: "비밀번호",
+        changeDescription: "***********",
+        changer: "홍길동",
+        changeTime: "2022-01-01 00:00:00"
+    }
+    ];
 
     return (
         <div className="activity-log">
             <h2>활동 로그</h2>
-            <table className='d-flex justify-content-start table table-hover'>
+            <table className='table table-hover'>
                 <tbody>
                     <tr>
-                        <th>최근 로그인 날짜</th>
-                        <th>주 게임 카테고리</th>
-                        <th>댓글 작성</th>
+                        <th className='w-[10%]'>변경 사항</th>
+                        <th className='w-[60%]'>변경 내용</th>
+                        <th className='w-[10%]'>변경 자</th>
+                        <th className='w-[20%]'>변경 시간</th>
                     </tr>
-                    <tr>
-                        <td>{activityLog.lastLogin}</td>
-                        <td>{activityLog.mainGameCategory}</td>
-                        <td>{activityLog.commentCount}</td>
-                    </tr>
+                    {activityLog.map((activity, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{activity.changeDetails}</td>
+                                <td>{activity.changeDescription}</td>
+                                <td>{activity.changer}</td>
+                                <td>{activity.changeTime}</td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </table>
 
