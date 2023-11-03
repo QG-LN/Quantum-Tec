b2e05c739f9b70e8de35f91eff81ea3d8b3337d6
YeChanKang <yechan.dev@gmail.com>
2023-11-04 00:58:04 +0900

    feat: [#125] 사용자 활동 로그를 담을 (response) UserActivityLogDTO 생성

---
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/Response/dashboard/UserActivityLogDTO.java
@@ -0,0 +1,8 @@
+package com.project.quantumtec.DTO.Response.dashboard;
+
+public class UserActivityLogDTO {
+    private String activityType;        // 활동 사항
+    private String activityData;        // 활동 내용
+    private String activityUser;        // 활동 사용자
+    private String activityTime;        // 활동 시간 (년-월-일 시:분:초)
+}
