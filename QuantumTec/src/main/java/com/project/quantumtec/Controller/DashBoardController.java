a22d662bd98cd627b7f82efd4ca8e74212280bbd
YeChanKang <yechan.dev@gmail.com>
2023-11-04 02:04:06 +0900

    feat: [#125] DashBoardController에 사용자 리스트 조회하는 메소드 추가

--- QuantumTec/src/main/java/com/project/quantumtec/Controller/DashBoardController.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Controller/DashBoardController.java
@@ -1,7 +1,9 @@
 package com.project.quantumtec.Controller;
 
 import com.project.quantumtec.DTO.Request.dashboard.UserDTO;
+import com.project.quantumtec.DTO.Request.dashboard.UserSearchDTO;
 import com.project.quantumtec.DTO.Response.dashboard.UserInfoDTO;
+import com.project.quantumtec.DTO.Response.dashboard.UserListDTO;
 import com.project.quantumtec.Service.dashboard.DashBoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,15 +11,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/dashboard")
 public class DashBoardController {
 
     @Autowired
     private DashBoardService dashBoardService;
+
     //프로필 정보 조회
     @PostMapping("/userinfo")
     public UserInfoDTO getUserInfo(@RequestBody UserDTO user){
         return dashBoardService.getUserInfo(user);
+    }
+
+    //사용자 리스트 조회
+    @PostMapping("/userlist")
+    public List<UserListDTO> getUserList(@RequestBody UserSearchDTO user){
+        return dashBoardService.getUserList(user);
     }
 }
