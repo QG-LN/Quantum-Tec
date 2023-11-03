8413e6f5b3329a1470ae62f2cd47f9e3629c539d
YeChanKang <yechan.dev@gmail.com>
2023-11-04 02:04:59 +0900

    feat: [#125] DashBoardService,Impl에 사용자 목록에 표시할 정보들을 불러오는 메서드 (사용자 리스트 & 추가 정보) 를 받아오는 메소드 추가

--- QuantumTec/src/main/java/com/project/quantumtec/Service/dashboard/DashBoardServiceImpl.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Service/dashboard/DashBoardServiceImpl.java
@@ -2,9 +2,13 @@ package com.project.quantumtec.Service.dashboard;
 
 import com.project.quantumtec.DAO.dashboard.DashBoardDAO;
 import com.project.quantumtec.DTO.Request.dashboard.UserDTO;
+import com.project.quantumtec.DTO.Request.dashboard.UserSearchDTO;
 import com.project.quantumtec.DTO.Response.dashboard.UserInfoDTO;
+import com.project.quantumtec.DTO.Response.dashboard.UserListDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("DashBoardServiceImpl")
 public class DashBoardServiceImpl implements DashBoardService{
@@ -15,5 +19,10 @@ public class DashBoardServiceImpl implements DashBoardService{
     @Override
     public UserInfoDTO getUserInfo(UserDTO user) {
         return dashBoardDAO.getUserInfo(user);
+    }
+
+    @Override
+    public List<UserListDTO> getUserList(UserSearchDTO user) {
+        return dashBoardDAO.getUserList(user);
     }
 }
