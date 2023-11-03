5c904a302677465198197d91151bea0dc551203e
YeChanKang <yechan.dev@gmail.com>
2023-11-04 01:00:39 +0900

    feat: [#125] 사용자 관리 페이지에서 사용자 선택 시 받아올 사용자 상세 정보를 담아 보낼 response 정보 (프로필 정보 & 사용자 보유 항목 중 레벨,경험치,무료캐시,유료캐시) 를 담을 UserInfoDTO 생성

---
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/Response/dashboard/UserInfoDTO.java
@@ -0,0 +1,28 @@
+package com.project.quantumtec.DTO.Response.dashboard;
+
+import java.util.List;
+
+public class UserInfoDTO {
+    // 프로필 정보
+    private String userNum;         // 사용자 번호
+    private String userName;        // 사용자 이름
+    private String userID;          // 사용자 아이디
+    private String userNickname;    // 사용자 닉네임
+    private String userGender;          // 사용자 성별
+    private String userAddress;         // 사용자 기본주소
+    private String userAddressDetail;   // 사용자 상세주소
+    private String userEmail;           // 사용자 이메일
+    private String userBirth;       // 사용자 생일
+    private String userJoinDate;    // 사용자 가입일
+    private String userUpdateDate;  // 사용자 정보 수정일
+    private String blackList;       // 사용자 블랙리스트 여부
+    private String userRole;            // 사용자 권한
+    private String userAttendanceCount; // 사용자 출석 횟수
+    private String userAvartaCount; // 사용자 아바타 개수
+    private String userBlockCount; // 사용자 차단 횟수
+    //  사용자 보유 항목 일부
+    private String userLevel;            // 사용자 레벨
+    private String userExp;            // 사용자 경험치
+    private String userFreeCash;            // 사용자 무료 캐시
+    private String userCash;            // 사용자 캐시
+}
