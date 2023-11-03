e14aab3802648cc82e4eafcfaffff4ee099f7090
YeChanKang <yechan.dev@gmail.com>
2023-11-04 01:08:56 +0900

    feat: [#125] request DTO인 UserInfoDTO를 UserDTO로 리펙터링 (response에도 동일한 이름의 DTO가 있어서 구분을 위해)

--- QuantumTec/src/main/java/com/project/quantumtec/DTO/Request/dashboard/UserDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/Request/dashboard/UserDTO.java
@@ -3,6 +3,6 @@ package com.project.quantumtec.DTO.Request.dashboard;
 import lombok.Data;
 
 @Data
-public class UserInfoDTO {
+public class UserDTO {
     private String userID;          // 사용자 아이디
 }
