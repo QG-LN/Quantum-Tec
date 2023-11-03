d8a6d3978d83e092be891ca818b6d137a2dc6e03
YeChanKang <yechan.dev@gmail.com>
2023-11-04 01:20:05 +0900

    feat: [#125] DashBoardDAOImpl 생성 후 사용자 프로필을 가져올 getUserInfo 메소드 생성

---
+++ QuantumTec/src/main/java/com/project/quantumtec/DAO/dashboard/DashBoardDAOImpl.java
@@ -0,0 +1,11 @@
+package com.project.quantumtec.DAO.dashboard;
+
+import com.project.quantumtec.DTO.Request.dashboard.UserDTO;
+import com.project.quantumtec.DTO.Response.dashboard.UserInfoDTO;
+
+public class DashBoardDAOImpl implements DashBoardDAO{
+    @Override
+    public UserInfoDTO getUserInfo(UserDTO user) {
+        return null;
+    }
+}
