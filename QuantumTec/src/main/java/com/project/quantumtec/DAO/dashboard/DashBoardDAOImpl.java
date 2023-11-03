7b3705aaf0748e30401d332c8bcf48089b0583d5
YeChanKang <yechan.dev@gmail.com>
2023-11-04 01:34:21 +0900

    feat: [#125] DashBoardDAOImpl 내부에 사용자 상세정보 페이지에 담길 프로필 정보를 요청하는 메소드 작성

--- QuantumTec/src/main/java/com/project/quantumtec/DAO/dashboard/DashBoardDAOImpl.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DAO/dashboard/DashBoardDAOImpl.java
@@ -2,12 +2,23 @@ package com.project.quantumtec.DAO.dashboard;
 
 import com.project.quantumtec.DTO.Request.dashboard.UserDTO;
 import com.project.quantumtec.DTO.Response.dashboard.UserInfoDTO;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class DashBoardDAOImpl implements DashBoardDAO{
+
+    @Autowired
+    private SqlSession sqlSession;
+
+    // 사용자 상세정보 페이지에 담길 프로필 정보를 받아오는 메소드
     @Override
     public UserInfoDTO getUserInfo(UserDTO user) {
-        return null;
+        try {
+            return sqlSession.selectOne("DashBoardDAO.getUserInfo", user);
+        }catch (Exception e){
+            return null;
+        }
     }
 }
