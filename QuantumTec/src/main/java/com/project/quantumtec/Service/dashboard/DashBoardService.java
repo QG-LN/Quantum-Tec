860301094d54199c5e1e945039a23e198f194818
YeChanKang <yechan.dev@gmail.com>
2023-11-04 01:12:17 +0900

    feat: [#125] DashBoardService를 클래스에서 인터페이스로 변경.

--- QuantumTec/src/main/java/com/project/quantumtec/Service/dashboard/DashBoardService.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Service/dashboard/DashBoardService.java
@@ -1,5 +1,10 @@
 package com.project.quantumtec.Service.dashboard;
 
-public class DashBoardService {
+import com.project.quantumtec.DTO.Request.dashboard.UserDTO;
+import com.project.quantumtec.DTO.Response.dashboard.UserInfoDTO;
 
+public interface DashBoardService {
+
+    // 사용자 상세 정보를 불러오는 메서드 (사용자 프로필만)
+    public UserInfoDTO getUserInfo(UserDTO user);
 }
