e214e1d75039202b2439034fdb916676de7f2f41
YeChanKang <yechan.dev@gmail.com>
2023-11-04 01:22:07 +0900

    feat: [#125] DashBoardController 내부에 DashBoardService 연결 후 사용자 프로필을 가져올 getUserInfo 메소드 생성

--- QuantumTec/src/main/java/com/project/quantumtec/Controller/DashBoardController.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Controller/DashBoardController.java
@@ -1,15 +1,23 @@
 package com.project.quantumtec.Controller;
 
+import com.project.quantumtec.DTO.Request.dashboard.UserDTO;
+import com.project.quantumtec.DTO.Response.dashboard.UserInfoDTO;
+import com.project.quantumtec.Service.dashboard.DashBoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/dashboard")
 public class DashBoardController {
-    //프로필 정보 조회
-    @PostMapping("/profile")
-    public void getProfile(){
 
+    @Autowired
+    private DashBoardService dashBoardService;
+    //프로필 정보 조회
+    @PostMapping("/userinfo")
+    public UserInfoDTO getUserInfo(@RequestBody UserDTO user){
+        return dashBoardService.getUserInfo(user);
     }
 }
