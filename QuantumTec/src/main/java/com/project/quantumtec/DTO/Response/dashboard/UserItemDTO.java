37fd44cc40257af401c932c92dab01d9f491fd45
YeChanKang <yechan.dev@gmail.com>
2023-11-04 01:01:18 +0900

    feat: [#125] 사용자 상세 정보 중 보유 항목을 response하기 위해 정보를 담을 UserItemDTO 생성

---
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/Response/dashboard/UserItemDTO.java
@@ -0,0 +1,12 @@
+package com.project.quantumtec.DTO.Response.dashboard;
+
+public class UserItemDTO {
+    private int itemNum;            // 아이템 번호
+    private String itemType;        // 아이템 종류
+    private String itemName;        // 아이템 이름
+    private String itemPrice;       // 아이템 가격
+    private String itemPayment;     // 아이템 결제 방식
+    private String itemPaymentStatus; // 아이템 결제 상태
+    private String itemPurchaseDate; // 아이템 구매 날짜
+
+}
