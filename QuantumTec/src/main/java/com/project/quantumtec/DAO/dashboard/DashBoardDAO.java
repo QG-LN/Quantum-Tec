12ab6cf0d0c8b39169d64f20f312e7f6d1c9c6e5
YeChanKang <yechan.dev@gmail.com>
2023-11-04 02:04:30 +0900

    feat: [#125] DashBoardDAO,Impl에 사용자 목록 페이지에 담길 사용자 리스트를 받아오는 메소드 추가

--- QuantumTec/src/main/java/com/project/quantumtec/DAO/dashboard/DashBoardDAO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DAO/dashboard/DashBoardDAO.java
@@ -1,9 +1,16 @@
 package com.project.quantumtec.DAO.dashboard;
 
 import com.project.quantumtec.DTO.Request.dashboard.UserDTO;
+import com.project.quantumtec.DTO.Request.dashboard.UserSearchDTO;
 import com.project.quantumtec.DTO.Response.dashboard.UserInfoDTO;
+import com.project.quantumtec.DTO.Response.dashboard.UserListDTO;
+
+import java.util.List;
 
 public interface DashBoardDAO {
     // 사용자 상세정보 요청시 프로필 영역 데이터를 가져오는 메소드
     UserInfoDTO getUserInfo(UserDTO user);
+
+    // 사용자 목록 요청시 사용자 리스트 데이터를 가져오는 메소드
+    List<UserListDTO> getUserList(UserSearchDTO user);
 }
--- QuantumTec/src/main/java/com/project/quantumtec/DAO/dashboard/DashBoardDAOImpl.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DAO/dashboard/DashBoardDAOImpl.java
@@ -1,10 +1,14 @@
 package com.project.quantumtec.DAO.dashboard;
 
 import com.project.quantumtec.DTO.Request.dashboard.UserDTO;
+import com.project.quantumtec.DTO.Request.dashboard.UserSearchDTO;
 import com.project.quantumtec.DTO.Response.dashboard.UserInfoDTO;
+import com.project.quantumtec.DTO.Response.dashboard.UserListDTO;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class DashBoardDAOImpl implements DashBoardDAO{
@@ -17,6 +21,16 @@ public class DashBoardDAOImpl implements DashBoardDAO{
     public UserInfoDTO getUserInfo(UserDTO user) {
         try {
             return sqlSession.selectOne("DashBoardDAO.getUserInfo", user);
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    // 사용자 목록 페이지에 담길 사용자 리스트를 받아오는 메소드
+    @Override
+    public List<UserListDTO> getUserList(UserSearchDTO user) {
+        try {
+            return sqlSession.selectList("DashBoardDAO.getUserList", user);
         }catch (Exception e){
             return null;
         }
