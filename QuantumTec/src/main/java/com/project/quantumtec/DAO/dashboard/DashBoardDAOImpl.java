4def6283b8138c0467f24ae97d6ed792c0944d38
YeChanKang <yechan.dev@gmail.com>
2023-11-04 01:22:59 +0900

    feat: [#125] DashBoardDAOImpl 내부에 누락되었던 @Repository 태그 추가 (DashBoardService에서 DashBoardDAO를 연결하지 못하는 오류 해결)

--- QuantumTec/src/main/java/com/project/quantumtec/DAO/dashboard/DashBoardDAOImpl.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DAO/dashboard/DashBoardDAOImpl.java
@@ -2,7 +2,9 @@ package com.project.quantumtec.DAO.dashboard;
 
 import com.project.quantumtec.DTO.Request.dashboard.UserDTO;
 import com.project.quantumtec.DTO.Response.dashboard.UserInfoDTO;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class DashBoardDAOImpl implements DashBoardDAO{
     @Override
     public UserInfoDTO getUserInfo(UserDTO user) {
