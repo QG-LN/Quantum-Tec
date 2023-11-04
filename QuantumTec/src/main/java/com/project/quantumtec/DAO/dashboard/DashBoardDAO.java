41a4c92104f622e275a86f2561a15f0d2689a441
MayoneJY <mayone6063@kakao.com>
2023-11-04 21:52:56 +0900

    fix: [#125] public 누락 수정.

--- QuantumTec/src/main/java/com/project/quantumtec/DAO/dashboard/DashBoardDAO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DAO/dashboard/DashBoardDAO.java
@@ -1,16 +1,17 @@
 package com.project.quantumtec.DAO.dashboard;
 
-import com.project.quantumtec.DTO.Request.dashboard.UserDTO;
+import com.project.quantumtec.DTO.Request.dashboard.UserIdDTO;
 import com.project.quantumtec.DTO.Request.dashboard.UserSearchDTO;
 import com.project.quantumtec.DTO.Response.dashboard.UserInfoDTO;
 import com.project.quantumtec.DTO.Response.dashboard.UserListDTO;
+import com.project.quantumtec.VO.dashboard.UserListVO;
 
 import java.util.List;
 
 public interface DashBoardDAO {
     // 사용자 상세정보 요청시 프로필 영역 데이터를 가져오는 메소드
-    UserInfoDTO getUserInfo(UserDTO user);
+    public UserInfoDTO getUserInfo(UserIdDTO user);
 
     // 사용자 목록 요청시 사용자 리스트 데이터를 가져오는 메소드
-    List<UserListDTO> getUserList(UserSearchDTO user);
+    public List<UserListVO> getUserList(UserSearchDTO user);
 }
