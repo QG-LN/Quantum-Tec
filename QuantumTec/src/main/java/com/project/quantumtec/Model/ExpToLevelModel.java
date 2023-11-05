46f58b2c0101f860113bc4f5230d92faa0bb33aa
MayoneJY <mayone6063@kakao.com>
2023-11-05 20:29:43 +0900

    fix: #[125] 폴더 위치 수정.

--- QuantumTec/src/main/java/com/project/quantumtec/Model/ExpToLevelModel.java
+++
@@ -1,9 +0,0 @@
-package com.project.quantumtec.Model;
-
-import lombok.Data;
-
-@Data
-public class ExpToLevelModel {
-    private int level;
-    private int required_exp;
-}
---
+++ QuantumTec/src/main/java/com/project/quantumtec/Model/exp/ExpToLevelModel.java
@@ -0,0 +1,10 @@
+package com.project.quantumtec.Model.exp;
+
+import lombok.Data;
+
+@Data
+public class ExpToLevelModel {
+    private int gameLevel;
+    private int gameRequiredExperience;
+
+}
