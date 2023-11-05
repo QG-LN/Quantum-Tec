52628dba1b16a3a6dcb3ae5a261ae00086dbcf89
MayoneJY <mayone6063@kakao.com>
2023-11-05 21:51:59 +0900

    fix: [#125] 명칭 변경.

--- QuantumTec/src/main/java/com/project/quantumtec/DTO/Response/dashboard/UserInfoDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/Response/dashboard/UserInfoDTO.java
@@ -10,7 +10,7 @@ public class UserInfoDTO {
     private String userUpdateDate;  // 사용자 정보 수정일
     private String userRole;            // 사용자 권한
     private int userAvartaCount; // 사용자 아바타 개수
-    private int userBlockCount; // 사용자 차단 횟수
+    private int userBannedCount; // 사용자 차단 횟수
     //  사용자 보유 항목 일부
     private int userFreeCash;            // 사용자 무료 캐시
 }
