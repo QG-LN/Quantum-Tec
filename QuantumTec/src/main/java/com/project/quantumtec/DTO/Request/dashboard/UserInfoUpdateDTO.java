d4eb033d6624d058d167b0951258eb3c81ae4db9
YeChanKang <yechan.dev@gmail.com>
2023-11-07 23:18:03 +0900

    feat: [#125] 사용자 정보 수정 요청이 들어왔을 때 수정된 데이터들을 담을 UserInfoUpdateDTO 생성

---
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/Request/dashboard/UserInfoUpdateDTO.java
@@ -0,0 +1,18 @@
+package com.project.quantumtec.DTO.Request.dashboard;
+
+import lombok.Data;
+
+@Data
+public class UserInfoUpdateDTO {
+    // 프로필 정보
+    private int userNum;         // 사용자 번호(수정불가, 고유값)
+    private String userName;        // 사용자 이름
+    private String userNickname;    // 사용자 닉네임
+    private String userGender;          // 사용자 성별
+    private String userAddress;         // 사용자 기본주소
+    private String userAddressDetail;   // 사용자 상세주소
+    private String userEmail;           // 사용자 이메일
+    private String userBirth;       // 사용자 생일
+    private String blackList;       // 사용자 블랙리스트 여부
+    private String userRole;            // 사용자 권한
+}
