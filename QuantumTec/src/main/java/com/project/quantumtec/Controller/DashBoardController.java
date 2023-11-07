d79bc9023f4813ad58d563ff32772741fc9c831b
YeChanKang <yechan.dev@gmail.com>
2023-11-07 22:51:57 +0900

    feat: [#125] DashBoardController 내부에 메소드 추가. 1.프로필 정보 변경 2.사용자 활성/비활성화 변환 3.계정 삭제 4.계정 비밀번호 초기화 5.사용자 보유 항목 리스트 전체 불러오기 6.사용자 활동 로그 항목별 1개씩 불러오기 7.사용자 활동 로그 전체 불러오기

--- QuantumTec/src/main/java/com/project/quantumtec/Controller/DashBoardController.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Controller/DashBoardController.java
@@ -1,14 +1,16 @@
 package com.project.quantumtec.Controller;
 
 import com.project.quantumtec.DTO.Request.dashboard.UserIdDTO;
+import com.project.quantumtec.DTO.Request.dashboard.UserItemSearchDTO;
+import com.project.quantumtec.DTO.Response.dashboard.UserActivityLogDTO;
 import com.project.quantumtec.DTO.Response.dashboard.UserInfoDTO;
+import com.project.quantumtec.DTO.Response.dashboard.UserItemDTO;
 import com.project.quantumtec.DTO.Response.dashboard.UserListDTO;
 import com.project.quantumtec.Service.dashboard.DashBoardService;
+import com.project.quantumtec.Service.user.UserService;
+import com.project.quantumtec.VO.user.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +20,7 @@ public class DashBoardController {
 
     @Autowired
     private DashBoardService dashBoardService;
+    private UserService userService;
 
     //프로필 정보 조회
     @PostMapping("/userinfo")
@@ -25,9 +28,49 @@ public class DashBoardController {
         return dashBoardService.getUserInfo(user);
     }
 
+    //프로필 정보 변경
+    @PostMapping("/userinfo/update")
+    public boolean updateUserInfo(@RequestBody UserInfoDTO user) throws Exception{
+        return dashBoardService.updateUserInfo(user);
+    }
+    //계정 활성화/비활성화 변환
+    @PostMapping("/userinfo/convertuserstatus")
+    public String convertUserStatus(@RequestBody UserIdDTO user) throws Exception{
+        return dashBoardService.convertUserStatus(user);
+    }
+    //계정 삭제
+    @DeleteMapping("/userinfo/delete")
+    public boolean deleteUser(@RequestParam int userIdx)throws Exception{
+        return userService.deleteUser(userIdx);
+    }
+
+    //계정 비밀번호 초기화
+    @PostMapping("/userinfo/resetpassword")
+    public boolean resetPassword(@RequestBody UserVO user)throws Exception{
+        return userService.findPw(user.getUserName(), user.getUserEmail(),  user.getUserID());
+    }
+
+    //사용자 보유 항목 리스트 불러오기 (모든 아이템)
+    @PostMapping("/userinfo/itemlist")
+    public List<UserItemDTO> getUserItemList(@RequestBody UserItemSearchDTO user) throws Exception{
+        return dashBoardService.getUserItemList(user);
+    }
+
+    //사용자 활동 로그 항목별 일부 불러오기
+    @PostMapping("/userinfo/activitylog")
+    public List<UserActivityLogDTO> getUserActivityLog(@RequestBody UserIdDTO user) throws Exception{
+        return dashBoardService.getUserActivityLog(user);
+    }
+
+    //사용자 활동 로그 전체 불러오기
+    @PostMapping("/userinfo/activitylogdetail")
+    public List<UserActivityLogDTO> getUserActivityLogDetail(@RequestBody UserIdDTO user) throws Exception{
+        return dashBoardService.getUserActivityLogDetail(user);
+    }
+
     //사용자 리스트 조회
     @RequestMapping("/userlist")
-    public List<UserListDTO> getUserList(){
+    public List<UserListDTO> getUserList() throws Exception{
         return dashBoardService.getUserList();
     }
 }
