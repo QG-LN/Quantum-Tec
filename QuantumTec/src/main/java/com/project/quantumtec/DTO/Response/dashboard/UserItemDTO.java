f445ec81cd802d46e90ae9a075e8411f275b63f7
MayoneJY <mayone6063@kakao.com>
2023-11-12 03:18:57 +0900

    modified: [#125] DTO수정.

--- QuantumTec/src/main/java/com/project/quantumtec/DTO/Response/dashboard/UserItemDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/DTO/Response/dashboard/UserItemDTO.java
@@ -4,12 +4,12 @@ import lombok.Data;
 
 @Data
 public class UserItemDTO {
-    private int itemNum;            // 아이템 번호
-    private String itemType;        // 아이템 종류
-    private String itemName;        // 아이템 이름
-    private int itemPrice;       // 아이템 가격
-    private String itemPayment;     // 아이템 결제 방식
-    private String itemPaymentStatus; // 아이템 결제 상태
-    private String itemPurchaseDate; // 아이템 구매 날짜
+    private int paymentIndex;            // 구매 번호
+    private String productType;        // 상품 종류
+    private String productName;        // 상품 이름
+    private int paymentAmount;       // 구매 가격
+    private String paymentMethod;     // 구매 결제 방식
+    private String paymentStatus; // 구매 결제 상태
+    private String paymentDate; // 구매 날짜
 
 }
