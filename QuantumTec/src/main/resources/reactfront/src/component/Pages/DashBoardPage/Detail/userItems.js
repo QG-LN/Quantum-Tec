defc42d5418c578c6c6185a08e01176021e675c3
MayoneJY <mayone6063@kakao.com>
2023-11-12 20:14:51 +0900

    feat: [#125] table 형식이 아닌 table템플릿 형식으로 변경

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/userItems.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/userItems.js
@@ -1,10 +1,20 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import {axiosRequest} from '../../../Utils/networkUtils';
+import TablePage from '../userPage';
+import ItemTableRow from '../../item-table-row';
 function UserItems({state}) {
     const [filter, setFilter] = useState("all");
     const [userItems, setUserItems] = useState([]);
-
+    const itemHeadLabel = [
+        { id: 'paymentIndex', label: '번호', align: 'center' },
+        { id: 'productType', label: '상품 종류', align: 'center' },
+        { id: 'productName', label: '상품 명칭', align: 'center' },
+        { id: 'paymentAmount', label: '결제 금액', align: 'center' },
+        { id: 'paymentMethod', label: '결제 수단', align: 'center' },
+        { id: 'paymentStatus', label: '결제 상태', align: 'center' },
+        { id: 'paymentDate', label: '결제 일자', align: 'center' },
+    ];
     useEffect(() => {
         if (!state.userIndex) {
             // userIndex가 없다면, 요청을 보내지 않습니다.
@@ -53,32 +63,9 @@ function UserItems({state}) {
             <input className='form-check-input m-2' type="radio" name="filter" value="avatar" checked={filter === "avatar"} onChange={(e) => setFilter(e.target.value)} /> 아바타
             <input className='form-check-input m-2' type="radio" name="filter" value="game" checked={filter === "game"} onChange={(e) => setFilter(e.target.value)} /> 게임
         </div>
-        <table className='table text-center align-middle'>
-            <tbody>
-                <tr>
-                    <th className='w-[5%]'>번호</th>
-                    <th className='w-[10%]'>상품 종류</th>
-                    <th className='w-[20%]'>상품 명칭</th>
-                    <th className='w-[10%]'>결제 금액</th>
-                    <th className='w-[10%]'>결제 수단</th>
-                    <th className='w-[10%]'>결제 상태</th>
-                    <th className='w-[15%]'>결제 일자</th>
-                    <th>비고</th>
-                </tr>
-            {filteredPayments.map(payment => (
-                <tr key={payment.paymentIndex}>
-                    <td>{payment.paymentIndex}</td>
-                    <td>{payment.productType}</td>
-                    <td>{payment.productName}</td>
-                    <td>{payment.paymentAmount}</td>
-                    <td>{payment.paymentMethod}</td>
-                    <td>{payment.paymentStatus}</td>
-                    <td>{payment.paymentDate.split(" ")[0]}</td>
-                    <td></td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
+
+        <TablePage margin={false} createButton={false} title={""} dataRow={ItemTableRow} dataLabel={itemHeadLabel} data={filteredPayments} />
+        
         </div>
     );
 }
---
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/item-table-row.jsx
@@ -0,0 +1,52 @@
+
+import TableRow from '@mui/material/TableRow';
+import Checkbox from '@mui/material/Checkbox';
+import TableCell from '@mui/material/TableCell';
+// ----------------------------------------------------------------------
+
+export default function ItemTableRow({row, selected, handleClick
+}) {
+  const paymentIndex = row.paymentIndex;
+  const productType = row.productType;
+  const productName = row.productName;
+  const paymentAmount = row.paymentAmount;
+  const paymentMethod = row.paymentMethod;
+  const paymentStatus = row.paymentStatus;
+  const paymentDate = row.paymentDate.split(" ")[0];
+
+
+
+  return (
+    <>
+      <TableRow hover tabIndex={-1} role="checkbox" selected={selected}>
+        <TableCell padding="checkbox">
+          <Checkbox disableRipple checked={selected} onChange={handleClick} />
+        </TableCell>
+
+        <TableCell align='center'>{paymentIndex}</TableCell>
+
+        <TableCell align='center'>{productType}</TableCell>
+
+        <TableCell align="center">
+          {productName}
+        </TableCell>
+
+        <TableCell align='center'>
+          {paymentAmount}
+        </TableCell>
+
+        <TableCell align='center'>
+          {paymentMethod}
+        </TableCell>
+
+        <TableCell align='center'>
+          {paymentStatus}
+        </TableCell>
+
+        <TableCell align='center'>
+          {paymentDate}
+        </TableCell>
+      </TableRow>
+    </>
+  );
+}
