ed198424b0b9a83cbc36f046be8a60a2506a564c
MayoneJY <mayone6063@kakao.com>
2023-11-12 23:06:32 +0900

    feat: [#125] 더미데이터 삭제 및 실제 데이터 추가.

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/logDetail.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/logDetail.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TablePage from '../userPage';
 import LogDetailTableRow from '../../log-detail-table-row';
-
-function LogDetail() {
+import {axiosRequest} from '../../../Utils/networkUtils';
+function LogDetail({state}) {
     const logDetailHeadLabel = [
         { id: 'title', label: '활동사항', align: 'center' },
-        { id: 'content', label: '활동내용' },
+        { id: 'content', label: '활동내용', align: 'center' },
         { id: 'changer', label: '활동자', align: 'center' },
         { id: 'changeDate', label: '활동 시간', align: 'center'}
     ];
 
-    const logDetailData = [
-        { title: '게시글', content: 'title: 제목 -> 제목1', changer: '홍길동', changeDate: '2022-01-01 00:00:00' },
-        { title: '게시글', content: 'content: 내용 -> 아니에요', changer: 'admin', changeDate: '2022-01-01 00:00:00' },
-        { title: '게시글', content: 'content: ㅁㄴㅇㄴㅁㅁㄴㅇ -> 내용', changer: '홍길동', changeDate: '2022-01-01 00:00:00' }
-    ];
-
+    const [logData, setLogData] = useState([]);
+    console.log(state.userId)
+    useEffect(() => {
+        const path = 'dashboard/userinfo/activitylogdetail';
+        const body = {
+            userId: state.userID
+        };
+        axiosRequest(path, body, 'POST', 'json')
+            .then((response) => {
+                console.log(response);
+                setLogData(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
     return (
         <>
-            <TablePage margin={false} createButton={false}  title={"활동 사항"} dataRow={LogDetailTableRow} dataLabel={logDetailHeadLabel} data={logDetailData} />
+            <TablePage margin={false} createButton={false} title={"활동 사항"} dataRow={LogDetailTableRow} dataLabel={logDetailHeadLabel} data={logData} />
         </>
     );
 }
