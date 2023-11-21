274742d6ef19eae1152d9c267aeb07a862e0f0db
YeChanKang <yechan.dev@gmail.com>
2023-11-22 04:28:41 +0900

    feat: [#135] 게임 상세정보를 수정하기 위해 Request 정보를 담을 GameInfoUpdateDTO 생성

---
+++ QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Request/dashboard/game/GameInfoUpdateDTO.java
@@ -0,0 +1,27 @@
+package com.project.quantumtec.Model.dto.Request.dashboard.game;
+
+import com.project.quantumtec.Model.dto.game.GameCommentDTO;
+import lombok.Data;
+
+@Data
+public class GameInfoUpdateDTO {
+    private String gameName; // 게임 이름
+    private int gamePrice; // 게임 가격
+    private int gameCategoryId; // 게임 카테고리 번호
+
+    // 우측 상자에 표시될 정보
+    private String gamePlatform; // 게임 플랫폼
+    private String gameVersion; // 게임 버전
+    private String gameVersionUpdateDate; // 게임 버전 업데이트 날짜
+    private String gameShortDescription; // 게임 짧은 설명
+    private String gameMemo; // 게임 메모
+
+    // 게임 상세 정보
+    private String gameImageLocation; // 게임 이미지 위치
+    private int gamePurchasePrice; // 게임 구매 가격
+    private String gameDescription; // 게임 설명
+    private String gameSaleInfo; // 게임 할인 정보
+    private String gameSaveDirectory; // 게임 저장 위치
+
+    private String gameVideoLocation; // 게임 비디오 위치
+}
