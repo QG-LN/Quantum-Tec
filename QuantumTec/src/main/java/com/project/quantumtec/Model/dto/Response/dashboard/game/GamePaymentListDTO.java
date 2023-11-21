d7880164a361552399833fd429100dd35f626aac
YeChanKang <yechan.dev@gmail.com>
2023-11-22 04:03:41 +0900

    feat: [#135] 특정 게임의 결제 리스트를 담을 GamePaymentListDTO 생성

---
+++ QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/game/GamePaymentListDTO.java
@@ -0,0 +1,13 @@
+package com.project.quantumtec.Model.dto.Response.dashboard.game;
+
+import lombok.Data;
+
+@Data
+public class GamePaymentListDTO {
+    private int paymentIndex;            // 구매 번호
+    private String userId;        // 사용자 아이디
+    private int paymentAmount;       // 구매 가격
+    private String paymentMethod;     // 결제 수단
+    private String paymentStatus; // 결제 상태
+    private String paymentDate; // 결제 날짜
+}
