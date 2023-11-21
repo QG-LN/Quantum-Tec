e860631a9df46bb5a0b1c18c3b6a3a881d3a13bd
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-11-21 20:19:51 +0900

    design[#134]: 버튼 디자인 수정

--- QuantumTec/src/main/resources/reactfront/src/component/exportData/exportData.js
+++ QuantumTec/src/main/resources/reactfront/src/component/exportData/exportData.js
@@ -2,22 +2,23 @@ import React, { useRef } from "react";
 import { CSVLink } from "react-csv";
 
 /**
- * 파일명, 데이터, 헤더를 받아서 엑셀 파일을 다운로드한다.
- * @param {} param0  fileName : 파일명, data : 데이터, header : 헤더
+ * 버튼명, 파일명, 데이터, 헤더를 받아서 엑셀 파일을 다운로드한다.
+ * @param {} param0 title:버튼명 fileName : 파일명, data : 데이터, header : 헤더
  * @returns  엑셀 파일 다운로드 컴포넌트
  */
-function ExportDataToExcel({ fileName , data , header}) {
+function ExportDataToExcelButton({ title,fileName , data , header}) {
   const csvLink = useRef();
 
   const handleDownload = () => {
-    console.log("1111111111111");
     csvLink.current.link.click();
   };
 
   return (
     <div>
-      <button onClick={handleDownload}>
-        테스트 엑셀 파일 다운로드
+      <button onClick={handleDownload}
+        className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full"
+      >
+        {title}
       </button>
       <CSVLink
         data={data}
@@ -31,4 +32,4 @@ function ExportDataToExcel({ fileName , data , header}) {
   );
 }
 
-export default ExportDataToExcel;
+export default ExportDataToExcelButton;
