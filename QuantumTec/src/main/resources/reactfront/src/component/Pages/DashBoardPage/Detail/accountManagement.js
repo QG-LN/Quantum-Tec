a2b9ef9343ab5e0a8c5f732b5480d7e9cc22c230
MayoneJY <mayone6063@kakao.com>
2023-11-21 22:29:40 +0900

    feat: [#125] 계정 활성화/비활성화 기능 추가.

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/accountManagement.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/accountManagement.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {axiosRequest} from '../../../Utils/networkUtils';
 import { useState } from 'react';
 
-function AccountManagement({state}) {
+function AccountManagement({state, setState}) {
   const [boolButtonDisabled, setBoolButtonDisabled] = useState(false);
 
   const clickPasswordReset = () => {
@@ -27,10 +27,38 @@ function AccountManagement({state}) {
       });
   }
 
+  const clickAccountActivation = () => {
+    setBoolButtonDisabled(true);
+    const path = 'dashboard/userinfo/convertuserstatus';
+    const body = {}
+    if(state.userStatus === "active"){
+      body.userIndex = state.userIndex;
+      body.banReason = prompt("사유를 입력해주세요.", "관리자가 계정을 비활성화함");
+      body.banSchedule = parseInt(prompt("비활성화 기간을 입력해주세요.", 5));
+    }
+    else{
+      body.userIndex = state.userIndex;
+    }
+    console.log(body)
+    axiosRequest(path, body, 'POST', 'json')
+      .then((response) => {
+        if(response){
+          alert('계정을 활성화/비활성화 했습니다.');
+          setState(prevState => ({ ...prevState, userStatus: prevState.userStatus === "active"?"banned":"active" }))
+        }
+        else
+          alert('계정 활성화/비활성화에 실패했습니다.');
+        setBoolButtonDisabled(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <div className="account-management d-flex justify-content-end">
       <button className='btn btn-danger m-2' onClick={clickPasswordReset} disabled={boolButtonDisabled}>비밀번호 초기화</button>
-      <button className='btn btn-danger m-2' onClick={() => {/* 계정 활성화/비활성화 로직 */}} disabled={boolButtonDisabled}>계정 비활성화</button>
+      <button className='btn btn-danger m-2' onClick={clickAccountActivation} disabled={boolButtonDisabled}>계정 {state.userStatus === "active"?"비":""}활성화</button>
       {/* <button className='btn btn-danger m-2' onClick={() => {}}>계정 삭제</button> */}
     </div>
   );
