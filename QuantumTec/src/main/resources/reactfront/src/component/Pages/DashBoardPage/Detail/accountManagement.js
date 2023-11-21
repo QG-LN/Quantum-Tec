b3f8075fcbee428de2c201448997f04352411874
MayoneJY <mayone6063@kakao.com>
2023-11-21 20:06:10 +0900

    feat: [#125] 비밀번호 초기화 기능 추가.
    해결해야할 일:
    프로필이 수정된 후 초기화 버튼을 누르면 오류 발생(기존 데이터와 DB값이 일치하지 않아서)

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/accountManagement.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/accountManagement.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import {axiosRequest} from '../../../Utils/networkUtils';
+import { useState } from 'react';
 
-function AccountManagement() {
+function AccountManagement({state}) {
+  const [boolButtonDisabled, setBoolButtonDisabled] = useState(false);
+
+  const clickPasswordReset = () => {
+    setBoolButtonDisabled(true);
+    const path = 'dashboard/userinfo/resetpassword';
+    const body = {
+      userID: state.userID,
+      userName: state.userName,
+      userEmail: state.userEmail
+    }
+    console.log(body)
+    axiosRequest(path, body, 'POST', 'json')
+      .then((response) => {
+        if(response)
+          alert('초기화된 비밀번호를 이메일로 전송했습니다.');
+        else
+          alert('비밀번호 초기화에 실패했습니다.');
+        setBoolButtonDisabled(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <div className="account-management d-flex justify-content-end">
-      <button className='btn btn-danger m-2' onClick={() => {/* 비밀번호 초기화 로직 */}}>비밀번호 초기화</button>
-      <button className='btn btn-danger m-2' onClick={() => {/* 계정 활성화/비활성화 로직 */}}>계정 비활성화</button>
-      <button className='btn btn-danger m-2' onClick={() => {/* 계정 삭제 로직 */}}>계정 삭제</button>
+      <button className='btn btn-danger m-2' onClick={clickPasswordReset} disabled={boolButtonDisabled}>비밀번호 초기화</button>
+      <button className='btn btn-danger m-2' onClick={() => {/* 계정 활성화/비활성화 로직 */}} disabled={boolButtonDisabled}>계정 비활성화</button>
+      {/* <button className='btn btn-danger m-2' onClick={() => {}}>계정 삭제</button> */}
     </div>
   );
 }
