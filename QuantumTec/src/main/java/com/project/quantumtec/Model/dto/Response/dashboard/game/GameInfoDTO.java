ff97796a45179b2df679c57071f681bc10469a13
YeChanKang <yechan.dev@gmail.com>
2023-11-22 15:38:19 +0900

    feat: [#135] GameInfoDTO 수정 GameInfoDTO 내부의 변수중에 데이터 후처리 과정이 필요한 것들을 주석으로 분리하여 표기

--- QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/game/GameInfoDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/game/GameInfoDTO.java
@@ -1,9 +1,6 @@
 package com.project.quantumtec.Model.dto.Response.dashboard.game;
 
-import com.project.quantumtec.Model.dto.game.GameCommentDTO;
 import lombok.Data;
-
-import java.util.List;
 @Data
 public class GameInfoDTO {
     // 게임 상세 정보
@@ -15,11 +12,13 @@ public class GameInfoDTO {
     private String gameSaveDirectory; // 게임 저장 위치
     private String gameVideoLocation; // 게임 비디오 위치
     private String gamePurchaseTrend; // 게임 구매 추이
-    private int gameAccessByTime; // 시간대 별 게임 접속량
+    private String gameRefund; // 게임 환불
+    private String gameReport; // 게임 신고
+
+    // 데이터 후처리 필요
+    private GameTimeDTO gameAccessByTime; // 시간대 별 게임 접속량
     private int gameAccessByDate; // 일일 게임 접속량
     private int gameCommentCount; // 게임 댓글 개수
     private int gameRatingVolatility; // 게임 평점 변동성
     private int gameTopRankTime; // 게임 탑 랭킹 시간
-    private String gameRefund; // 게임 환불
-    private String gameReport; // 게임 신고
 }
