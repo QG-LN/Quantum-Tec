a8f45ba65c73b6aee40d17a32da6cf0ede77b72c
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-11-23 02:11:39 +0900

    feat[#134]: json데이터를 csv로 변환
    papaparse라이브러리를 이용하여 json데이터를 알맞게 변환

--- QuantumTec/src/main/resources/reactfront/src/component/exportData/exportData.js
+++ QuantumTec/src/main/resources/reactfront/src/component/exportData/exportData.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { CSVLink } from "react-csv";
+import Papa from "papaparse";
 
 /**
  * 버튼명, 파일명, 데이터, 헤더를 받아서 엑셀 파일을 다운로드한다.
@@ -16,7 +17,7 @@ function ExportDataToExcelButton({ title,fileName , data , header}) {
   /**
    * 현재 날짜 데이터를 반환하는 함수
    * @returns 2021-08-31-오후-01-00-00 형식의 날짜
-   * @example 2021-08-31-오후-01-00-00
+   * @example getNowData() -> 2021-08-31-오후-01-00-00
    */
   function getNowData(){
     const now = new Date();
@@ -29,6 +30,9 @@ function ExportDataToExcelButton({ title,fileName , data , header}) {
     return `${date}-${ampm}-${hours}-${minutes}-${seconds}`;
   };
 
+  // JSON 데이터를 CSV로 변환
+  const csvData = Papa.unparse(data);
+
   return (
     <div>
       <button onClick={handleDownload}
@@ -37,7 +41,7 @@ function ExportDataToExcelButton({ title,fileName , data , header}) {
         {title}
       </button>
       <CSVLink
-        data={data}
+        data={csvData}
         headers={header}
         filename={fileName + "[" + getNowData() + "].csv"}
         className="hidden"
