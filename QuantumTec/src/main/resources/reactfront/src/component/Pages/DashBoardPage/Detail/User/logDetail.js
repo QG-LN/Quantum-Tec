c2f4ab57d7101120a360ae04a7e21e8311c0dd36
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-12-02 23:30:11 +0900

    fix[#126]: 활동사항 엑셀 다운로드 버튼이 노출되지 않는 오류 해결
    이름 띄어쓰기 문제

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/User/logDetail.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/User/logDetail.js
@@ -5,16 +5,16 @@ import {axiosRequest} from '../../../../Utils/networkUtils';
 import ActivityGraph from './activityGraph';
 function LogDetail({state}) {
     const logDetailHeadLabel = [
-        { id: 'activeTitle', label: '활동사항', align: 'center' },
-        { id: 'activeContent', label: '활동내용', align: 'center' },
-        { id: 'activeChanger', label: '활동자', align: 'center' },
-        { id: 'activeChangeDate', label: '활동시간', align: 'center'}
+        // { id: 'activeTitle', label: '활동사항', align: 'center' },
+        // { id: 'activeContent', label: '활동내용', align: 'center' },
+        // { id: 'activeChanger', label: '활동자', align: 'center' },
+        // { id: 'activeChangeDate', label: '활동시간', align: 'center'}
 
         // mayone
-        // { id: 'columnName', label: '활동사항', align: 'center' },
-        // { id: 'newValue', label: '활동내용', align: 'center' },
-        // { id: 'operatedBy', label: '활동자', align: 'center' },
-        // { id: 'timestamp', label: '활동 시간', align: 'center'}
+        { id: 'columnName', label: '활동사항', align: 'center' },
+        { id: 'newValue', label: '활동내용', align: 'center' },
+        { id: 'operatedBy', label: '활동자', align: 'center' },
+        { id: 'timestamp', label: '활동시간', align: 'center'}
     ];
 
     const [logData, setLogData] = useState([]);
@@ -39,7 +39,7 @@ function LogDetail({state}) {
             {/* <TablePage margin={false} createButton={false} title={"활동사항_"+state.userName} dataRow={LogDetailTableRow} dataLabel={logDetailHeadLabel} data={logData} /> */}
 
             { state?.userID && (
-                <TablePage margin={false} createButton={false} title={"활동 사항_"+state.userName} dataRow={LogDetailTableRow} dataLabel={logDetailHeadLabel} data={logData} />
+                <TablePage margin={false} createButton={false} title={"활동사항_"+state.userName} dataRow={LogDetailTableRow} dataLabel={logDetailHeadLabel} data={logData} />
             )}
 
             <div className='m-5'></div>
