1b677cdd8164cb158d99c4e8cf01997dc1c103c0
MayoneJY <mayone6063@kakao.com>
2023-12-03 18:35:47 +0900

    modified: [#135] 구현하기 힘들거나 없는 기능 주석처리

--- QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/game/GameInfoDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/game/GameInfoDTO.java
@@ -13,10 +13,10 @@ public class GameInfoDTO {
     private int gamePurchasePrice; // 게임 구매 가격
     private String gameDescription; // 게임 설명
     private String gameSaleInfo; // 게임 할인 정보
-    private String gameSaveDirectory; // 게임 저장 위치
-    private String gameVideoLocation; // 게임 비디오 위치
-    private String gamePurchaseTrend; // 게임 구매 추이
+    // private String gameSaveDirectory; // 게임 저장 위치
+    // private String gameVideoLocation; // 게임 비디오 위치
+    // private String gamePurchaseTrend; // 게임 구매 추이
     private String gameRefund; // 게임 환불
-    private String gameReport; // 게임 신고
+    // private String gameReport; // 게임 신고
     private int gameCommentCount; // 게임 댓글 작성량
 }
