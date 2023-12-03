dc156f7572d02e6fcd898b11b74a711917b17226
MayoneJY <mayone6063@kakao.com>
2023-12-03 18:38:11 +0900

    modified: [#135] 게임 할인 정보 세분화.

--- QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/game/GameInfoDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/game/GameInfoDTO.java
@@ -12,7 +12,10 @@ public class GameInfoDTO {
     private int gameSales; // 게임 판매량
     private int gamePurchasePrice; // 게임 구매 가격
     private String gameDescription; // 게임 설명
-    private String gameSaleInfo; // 게임 할인 정보
+    // private String gameSaleInfo; // 게임 할인 정보
+    private int gameDiscountRate; // 게임 할인율
+    private String gameDiscountStartDate; // 게임 할인 시작일
+    private String gameDiscountEndDate; // 게임 할인 종료일
     // private String gameSaveDirectory; // 게임 저장 위치
     // private String gameVideoLocation; // 게임 비디오 위치
     // private String gamePurchaseTrend; // 게임 구매 추이
