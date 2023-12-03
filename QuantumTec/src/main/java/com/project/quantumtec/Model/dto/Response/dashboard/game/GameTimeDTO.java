f45fcc15982efd950b1159f9bce320d44c274a7f
YeChanKang <yechan.dev@gmail.com>
2023-12-03 16:52:22 +0900

    feat: [#135] GameTimeDTO 추가

---
+++ QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/game/GameTimeDTO.java
@@ -0,0 +1,21 @@
+package com.project.quantumtec.Model.dto.Response.dashboard.game;
+
+import lombok.Data;
+import lombok.Setter;
+
+import java.util.List;
+
+@Data
+public class GameTimeDTO {
+    private int[] time; // 게임 시간 (0시~23시)
+
+    public void init(){
+        time = new int[24];
+        for(int i = 0; i < 24; i++){
+            time[i] = 0;
+        }
+    }
+    public void setTime(int index, int value){
+        time[index] = value;
+    }
+}
