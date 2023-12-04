4db0272b11635be59ab9ad351a1aa26d89f5e496
YeChanKang <yechan.dev@gmail.com>
2023-12-04 18:06:16 +0900

    feat: [#144] 댓글 활동량을 담기 위한 BoardCommentActivityDTO 생성

---
+++ QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/board/BoardCommentActivityDTO.java
@@ -0,0 +1,8 @@
+package com.project.quantumtec.Model.dto.Response.dashboard.board;
+
+import lombok.Data;
+
+@Data
+public class BoardCommentActivityDTO {
+    private int commentActivityCount; // 댓글 활동 수
+}
