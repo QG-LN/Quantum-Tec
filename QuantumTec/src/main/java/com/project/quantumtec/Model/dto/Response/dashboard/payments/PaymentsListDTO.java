dbc4bd94da4826327a3fed1ca36d5e5a38733753
MayoneJY <mayone6063@kakao.com>
2023-12-04 11:47:03 +0900

    feat: [#143] 프론트에서 기능을 더 사용하기 위해 sql 추가.

--- QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/payments/PaymentsListDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/payments/PaymentsListDTO.java
@@ -10,4 +10,9 @@ public class PaymentsListDTO {
     private String paymentMethod;           // 결제 수단
     private String paymentDate;             // 결제 일자
     private String paymentStatus;           // 결제 상태
+
+    private int userIndex;              // 결제한 유저 번호
+    private String paymentCategory;         // 결제한 아이템 종류 (Avatar, Game, Cash)
+    private int paymentItemIndex;           // 결제한 아이템 번호 (있을 경우)
+    private int paymentPrice;               // 결제 금액
 }
