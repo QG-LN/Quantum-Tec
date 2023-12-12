c9e04dffd9c3d58ed8b871401b7ce13b90028c9f
YeChanKang <yechan.dev@gmail.com>
2023-12-13 01:03:22 +0900

    feat: [#149] AvatarUsageVolumeDTO 수정 아이템 미사용량 변수 추가

--- QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/avatar/AvatarUsageVolumeDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/avatar/AvatarUsageVolumeDTO.java
@@ -6,4 +6,5 @@ import lombok.Data;
 public class AvatarUsageVolumeDTO {
     private int paymentSuccessCount; // 결제 성공 횟수
     private int itemUsageCount; // 아이템 사용량
+    private int itemNotUsageCount; // 아이템 미사용량
 }
