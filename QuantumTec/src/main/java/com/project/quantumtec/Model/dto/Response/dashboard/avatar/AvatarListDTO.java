0ff21ddc3092eac9dc5a0da21e5574cb04d8db0a
YeChanKang <yechan.dev@gmail.com>
2023-12-13 00:34:05 +0900

    feat: [#149] 아바타 리스트 요청 DTO, 응답 DTO 수정 기존에 페이지네이션을 백엔드에서 처리하려 설계한 것이 변경되어 Request DTO는 삭제하고 Response쪽에는 카테고리 이름을 추가

--- QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/avatar/AvatarListDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/avatar/AvatarListDTO.java
@@ -6,4 +6,5 @@ import lombok.Data;
 public class AvatarListDTO {
     private int itemIndex; // 아이템 번호
     private String itemName; // 아이템 이름
+    private String itemCategoryName; // 아이템 카테고리 이름
 }
