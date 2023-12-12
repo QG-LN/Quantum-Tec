04f7d26d615a60156107872763bf7deac4509dca
YeChanKang <yechan.dev@gmail.com>
2023-12-13 01:57:23 +0900

    fix: [#149] AvatarDetailDTO 수정 오타 수정

--- QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/avatar/AvatarDetailDTO.java
+++ QuantumTec/src/main/java/com/project/quantumtec/Model/dto/Response/dashboard/avatar/AvatarDetailDTO.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class AvatarDetailDTO {
     private int itemPrice; // 아이템 가격
     private String userName; // 사용자 이름
-    private String itemCreateorName; // 아이템 제작자 이름
+    private String itemCreatorName; // 아이템 제작자 이름
     private String itemCreateDate; // 아이템 제작 날짜
     private String itemDesc; // 아이템 설명
 
