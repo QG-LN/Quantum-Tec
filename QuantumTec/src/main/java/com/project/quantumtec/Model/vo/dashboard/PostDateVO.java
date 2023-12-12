e450b6597e70fd0ea864480b12ff27c4e01062fd
YeChanKang <yechan.dev@gmail.com>
2023-12-12 22:47:50 +0900

    feat: [#145] PostDateVO 생성 게시글 조회수, 게시글 댓글 수를 담을 VO 생성

---
+++ QuantumTec/src/main/java/com/project/quantumtec/Model/vo/dashboard/PostDateVO.java
@@ -0,0 +1,9 @@
+package com.project.quantumtec.Model.vo.dashboard;
+
+import lombok.Data;
+
+@Data
+public class PostDateVO {
+    private int postViewCount; // 게시글 조회 수
+    private int postCommentCount; // 게시글 댓글 수
+}
