61fb265c57a9bbf47ec123adb8f761e829e2d7e2
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-12-13 22:49:47 +0900

    modify[#149]: 아바타 프로필 서버 데이터를 받아서 처리하도록 세팅

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/Avatar/avatarProfile.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/Avatar/avatarProfile.js
@@ -6,56 +6,44 @@ import ActivityGraph from './activityGraph';
 import { useLocation } from "react-router";
 import { Container } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import LogDetail from './avatarLogDetail';
 import {axiosRequest} from '../../../../Utils/networkUtils';
 import { useDispatch } from 'react-redux';
 import { setDashboardAvatarProfileList } from '../../../../../redux/actions/dashboardAvatarProfileAction.js';
 import { getData } from '../../../DashBoardPage/Data/loadTableDataList.js';
+
+
 function AvatarProfile() {
   const dispatch = useDispatch();
   const [itemsData, setItemsData] = useState([]);
   const { state } = useLocation();
-  console.log(state);
+
+  const [avatarInfo, setAvatarInfo] = useState({});
+
   useEffect(() => {
-    
-    getData("dashboard/avatar")
-      .then(data => {
+    getData("dashboard/avatarlist")
+      .then((data) => {
         setItemsData(data);
         dispatch(setDashboardAvatarProfileList(data));
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("데이터 로딩 중 오류 발생", error);
       });
+
+    // 상세 정보를 불러오기 위한 요청
+    const path = "dashboard/avatarinfo";
+    const body = {
+      itemIndex: state?.itemIndex,
+    };
+    axiosRequest(path, body, "POST", "json")
+      .then((response) => {
+        console.log(response);
+        setAvatarInfo(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
-  
-  
-  // const location = useLocation();
-  // const [state, setState] = useState(location.state ? location.state.row : {});
- 
-  const { id } = useParams();
-  const defaultState = { };
-  // const [state, setState] = useState(states ? states.filter(e => e.itemIndex === parseInt(id))[0] || defaultState : defaultState);
-  
-//   useEffect(() => {
-//     if (!state?.gameIndex) {
-//         // userIndex가 없다면, 요청을 보내지 않습니다.
-//         setState(loadState);
-//         return;
-//     }
-//     const path = 'dashboard/gameinfo';
-//     const body = {
-//       gameIndex: state?.gameIndex
-//     }
-//     axiosRequest(path, body, 'POST', 'json')
-//         .then((response) => {
-//             setState(prevState => ({ ...prevState, ...response }));
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//         });
-// }, [state?.gameIndex]);
 
   const Styles = styled("div")({
     "@media (min-width: 1200px)": {
@@ -63,26 +51,22 @@ function AvatarProfile() {
     },
   });
 
-  useEffect(() => {
-    console.log(state);
-  }, [state]);
   return (
     <Styles>
-
-        <Container maxWidth="xl" >
-            <div className='mt-[100px]'></div>
-            <div className='text-left'>
-                <ProfileInfo state={state} />
-                <div className='m-5'></div>
-                <AvatarItems state={state} />
-                {/* <div className='m-5'></div>
+      <Container maxWidth="xl">
+        <div className="mt-[100px]"></div>
+        <div className="text-left">
+          <ProfileInfo state={state} avatarInfo={avatarInfo}  />
+          <div className="m-5"></div>
+          <AvatarItems state={state} />
+          {/* <div className='m-5'></div>
                 <LogDetail state={state} /> */}
-                <div className='m-5'></div>
-                <ActivityGraph state={state} />
-                <div className='m-5'></div>
-                <AccountManagement state={state} />
-            </div>
-        </Container>
+          <div className="m-5"></div>
+          <ActivityGraph state={state} />
+          <div className="m-5"></div>
+          <AccountManagement state={state} />
+        </div>
+      </Container>
     </Styles>
   );
 }
