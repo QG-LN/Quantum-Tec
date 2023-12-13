3c0dd0030cd22aac97a63432ccf8694b50b655ce
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-12-14 01:51:10 +0900

    test: 메인페이지 더미데이터 테스트를 위한 수정

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/dashboardHome.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/dashboardHome.js
@@ -60,9 +60,9 @@ const dayChartData = [
     { date: '11/01/2003'},
 ];
 const dayChartDataGame = [
-    { gameName: "Game 1", value: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30]},
-    { gameName: "Game 2", value: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43]},
-    { gameName: "Game 3", value: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39]}
+    { gameName: "별에서 온 곱셈 공식", value: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30]},
+    { gameName: "삼각형 닮음 카드 게임", value: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43]},
+    { gameName: "제곱근 레이싱", value: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39]}
 ];
 
 const setGameData = () =>{
@@ -85,22 +85,22 @@ const dayUserAgeData = [
 
 //게임별 이용자 수
 const gameUserData =[
-    { label: "Game1", value: 20 },
-    { label: "Game2", value: 30 },
-    { label: "Game3", value: 26 },
-    { label: "Game4", value: 20 },
-    { label: "Game5", value: 30 },
-    { label: "Game6", value: 43 },
+    { label: "별에서 온 곱셈 공식", value: 20 },
+    { label: "삼각형 닮음 카드 게임", value: 30 },
+    { label: "제곱근 레이싱", value: 26 },
+    { label: "삼각형의 외심 내심 무게 중심 살펴보기", value: 20 },
+    { label: "균형 잡힌 저울(정수)", value: 30 },
+    { label: "칠교 놀이(상급)", value: 43 },
 ]
 
 //최근 결제 건
 //date 형식은 iso-8601형식으로 mapper할때 바꿔서 가져와야됨
 const orderTimelineData = [
-    {text:"Game1, Item1, $4220", date: "2021-12-06T05:40:44.408Z"},
+    {text:"별에서 온 곱셈 공식, 4000원", date: "2021-12-06T05:40:44.408Z"},
     {text:"아바타, 초록 바지, 1000캐시", date: "2021-12-09T05:20:44.408Z"},
-    {text:"Order #37745 from September", date: "2021-12-03T05:40:44.408Z"},
-    {text:"New order placed #XF-2356", date: "2021-12-03T05:40:44.408Z"},
-    {text:"New order placed #XF-2356", date: "2021-12-03T05:40:44.408Z"},
+    {text:"아바타, 갈색 배경, 2000캐시", date: "2021-12-03T05:40:44.408Z"},
+    {text:"칠교 놀이(상급), 10000원", date: "2021-12-03T05:40:44.408Z"},
+    {text:"삼각형의 외심 내심 무게 중심 살펴보기 1000원", date: "2021-12-03T05:40:44.408Z"},
 ]
 
 //튜터링 이용자 수
