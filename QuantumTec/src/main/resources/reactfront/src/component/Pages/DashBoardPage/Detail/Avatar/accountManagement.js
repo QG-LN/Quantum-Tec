5881b5b924fc089d1ce258d0b64aa6c9dd66c01e
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-12-14 03:04:57 +0900

    modify: 불필요한 항목 삭제

--- QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/Avatar/accountManagement.js
+++ QuantumTec/src/main/resources/reactfront/src/component/Pages/DashBoardPage/Detail/Avatar/accountManagement.js
@@ -3,7 +3,6 @@ import React from 'react';
 function AccountManagement() {
   return (
     <div className="account-management d-flex justify-content-end">
-      <button className='btn btn-danger m-2' onClick={() => {}}>아이템 수정</button>
       <button className='btn btn-danger m-2' onClick={() => {}}>아이템 삭제</button>
     </div>
   );
